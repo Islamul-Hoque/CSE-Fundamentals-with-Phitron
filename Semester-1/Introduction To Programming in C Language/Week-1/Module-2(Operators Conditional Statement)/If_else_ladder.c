4fa91bdc7d69f4fe1f66732faffa4e2e8a55abaa
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-12 01:42:48 +0600

    feat(Semester-1/Week-1/Module-2/conditional): add if-else ladder example

--- Semester-1/Introduction To Programming in C Language/Week-1/Module-2(Operators Conditional Statement)/If_else_ladder.c
+++ Semester-1/Introduction To Programming in C Language/Week-1/Module-2(Operators Conditional Statement)/If_else_ladder.c
@@ -1,20 +1,20 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
+int main() {
 
-//     int tk;
-//     scanf("%d", &tk);
+    int tk;
+    scanf("%d", &tk);
 
-//     // Decision making based on amount
-//     if (tk >= 100) {
-//         printf("Burger khabo\n");
-//     } else if (tk >= 50) {
-//         printf("Fuchka khabo\n");
-//     } else if (tk >= 20) {
-//         printf("Chips khabo\n");
-//     } else {
-//         printf("Kichui khabo na\n");
-//     }
+    // Decision making based on amount
+    if (tk >= 100) {
+        printf("Burger khabo\n");
+    } else if (tk >= 50) {
+        printf("Fuchka khabo\n");
+    } else if (tk >= 20) {
+        printf("Chips khabo\n");
+    } else {
+        printf("Kichui khabo na\n");
+    }
 
-//     return 0;
-// }
+    return 0;
+}
--- Semester-1/Introduction To Programming in C Language/Week-1/Module-2(Operators Conditional Statement)/if_else.c
+++ Semester-1/Introduction To Programming in C Language/Week-1/Module-2(Operators Conditional Statement)/if_else.c
@@ -1,13 +1,13 @@
-// #include <stdio.h>
-// int main()
-// {
-//     int a = 6;
+#include <stdio.h>
+int main()
+{
+    int a = 6;
 
-//     if (a > 5) {
-//         printf("hi\n");
-//     } else if (a > 3)
-//     {
-//         printf("bye\n"); // প্রথম শর্তই সত্য হয়েছে, else if আর execute হবে না
-//     }
-//     return 0;
-// }
+    if (a > 5) {
+        printf("hi\n");
+    } else if (a > 3)
+    {
+        printf("bye\n"); // প্রথম শর্তই সত্য হয়েছে, else if আর execute হবে না
+    }
+    return 0;
+}
