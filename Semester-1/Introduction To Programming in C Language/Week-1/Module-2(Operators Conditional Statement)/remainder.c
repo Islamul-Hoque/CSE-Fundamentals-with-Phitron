e6275736e66d69a33beea318fa322f1604a4e9fa
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-12 01:40:58 +0600

    feat(Semester-1/Week-1/Module-2/remainder): add remainder operator example

--- Semester-1/Introduction To Programming in C Language/Week-1/Module-2(Operators Conditional Statement)/remainder.c
+++ Semester-1/Introduction To Programming in C Language/Week-1/Module-2(Operators Conditional Statement)/remainder.c
@@ -1,9 +1,9 @@
-// #include <stdio.h>
-// int main ()
-// {
-//     int a = 18, b = 5, remainder;
-//     remainder = a % b;
-//     printf("The remainder of %d and %d is = %d", a, b, remainder);
+#include <stdio.h>
+int main ()
+{
+    int a = 18, b = 5, remainder;
+    remainder = a % b;
+    printf("The remainder of %d and %d is = %d", a, b, remainder);
 
-//     return 0;
-// }
+    return 0;
+}
