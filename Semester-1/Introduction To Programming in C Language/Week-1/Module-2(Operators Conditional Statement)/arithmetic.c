ec254d4b158f232b1e91757d65df504fa826cdfd
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-12 01:39:30 +0600

    feat(Semester-1/Week-1/Module-2): add arithmetic operations and division examples

--- Semester-1/Introduction To Programming in C Language/Week-1/Module-2(Operators Conditional Statement)/arithmetic.c
+++ Semester-1/Introduction To Programming in C Language/Week-1/Module-2(Operators Conditional Statement)/arithmetic.c
@@ -1,26 +1,26 @@
-// #include <stdio.h>
-// int main ()
-// {
+#include <stdio.h>
+int main ()
+{
 
-//     double d = 15/4;
-//     printf("%lf\n", d);
+    double d = 15/4;
+    printf("%lf\n", d);
 
-//     int a = 15, b = 5, sum, sub, mul, division;
-//     sum = a + b;
-//     sub = a - b;
-//     mul = a * b;
-//     division = a / b;
+    int a = 15, b = 5, sum, sub, mul, division;
+    sum = a + b;
+    sub = a - b;
+    mul = a * b;
+    division = a / b;
 
-//     printf("summation is = %d\nsubtraction is = %d\nmultiplication is = %d\ndivision is = %d\n\n", sum, sub, mul, division);
+    printf("summation is = %d\nsubtraction is = %d\nmultiplication is = %d\ndivision is = %d\n\n", sum, sub, mul, division);
 
-//     printf("The summation of %d and %d is = %d\n", a, b, sum);
-//     printf("The subtraction of %d and %d is = %d\n", a, b, sub);
-//     printf("The multiplication of %d and %d is = %d\n", a, b, mul);
-//     printf("The division of %d and %d is = %d\n\n", a, b, division);
+    printf("The summation of %d and %d is = %d\n", a, b, sum);
+    printf("The subtraction of %d and %d is = %d\n", a, b, sub);
+    printf("The multiplication of %d and %d is = %d\n", a, b, mul);
+    printf("The division of %d and %d is = %d\n\n", a, b, division);
 
-//     float c = 7,  division2;
-//     division2 = a / c;
-//     printf("Division2 is = %f\n", division2);
+    float c = 7,  division2;
+    division2 = a / c;
+    printf("Division2 is = %f\n", division2);
 
-//     return 0;
-// }
+    return 0;
+}
