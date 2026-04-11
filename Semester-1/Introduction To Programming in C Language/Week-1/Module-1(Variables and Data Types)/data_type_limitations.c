f0ad54634105918936323c2242d7dcb55571a71a
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-12 01:37:31 +0600

    feat(Semester-1/Week-1/Module-1): add examples for basic variables, data types, and input/output

--- Semester-1/Introduction To Programming in C Language/Week-1/Module-1(Variables and Data Types)/data_type_limitations.c
+++ Semester-1/Introduction To Programming in C Language/Week-1/Module-1(Variables and Data Types)/data_type_limitations.c
@@ -1,8 +1,8 @@
-// #include<stdio.h>
+#include<stdio.h>
 
-// int main(){
-//     long long int a = 1000000000000000000;
-//     double b = 123.123451;
-//     printf("%lld\n%lf", a, b);
-//     return 0;
-// }
+int main(){
+    long long int a = 1000000000000000000;
+    double b = 123.123451;
+    printf("%lld\n%lf", a, b);
+    return 0;
+}
