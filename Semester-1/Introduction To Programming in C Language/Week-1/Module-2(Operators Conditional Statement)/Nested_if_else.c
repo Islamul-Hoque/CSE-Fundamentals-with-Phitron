eb73f4104352c1817f9a2ac075a00738cde43aa3
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-12 01:45:13 +0600

    feat(Semester-1/Week-1/Module-2/nested if-else): demonstrate nested if-else logic

--- Semester-1/Introduction To Programming in C Language/Week-1/Module-2(Operators Conditional Statement)/Nested_if_else.c
+++ Semester-1/Introduction To Programming in C Language/Week-1/Module-2(Operators Conditional Statement)/Nested_if_else.c
@@ -1,21 +1,21 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int tk;
-//     scanf("%d", &tk);
+int main() {
+    int tk;
+    scanf("%d", &tk);
 
-//     // Decision making based on amount
-//     if (tk >= 5000) {
-//         printf("Cox'sbazar jabo\n");
+    // Decision making based on amount
+    if (tk >= 5000) {
+        printf("Cox'sbazar jabo\n");
 
-//         if (tk >= 10000) {
-//             printf("Saint Martin jabo\n");
-//         } else {
-//             printf("jabo na\n");
-//         }
-//     } else {
-//         printf("Kothao jabo na\n");
-//     }
+        if (tk >= 10000) {
+            printf("Saint Martin jabo\n");
+        } else {
+            printf("jabo na\n");
+        }
+    } else {
+        printf("Kothao jabo na\n");
+    }
 
-//     return 0;
-// }
+    return 0;
+}
