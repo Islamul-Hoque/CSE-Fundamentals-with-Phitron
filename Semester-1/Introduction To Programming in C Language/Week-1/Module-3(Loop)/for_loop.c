dde83e27d5e79a427c675142af1d2ff8834d06ef
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-13 02:00:26 +0600

    feat(Semester-1/Week-1/Module-3/for-loop): add for loop programs to print string and numbers from 1 to n

--- Semester-1/Introduction To Programming in C Language/Week-1/Module-3(Loop)/for_loop.c
+++ Semester-1/Introduction To Programming in C Language/Week-1/Module-3(Loop)/for_loop.c
@@ -1,22 +1,22 @@
-// // print string
-// #include <stdio.h>
-// int main(){
-//     int n;
-//     scanf("%d", &n);
-//     for(int i = 1; i <= n; i++){
-//         printf("%d ISHFAK\n", i);
-//     }
-//     return 0;
-// }
+// print string
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++){
+        printf("%d ISHFAK\n", i);
+    }
+    return 0;
+}
 
-// // Print 1 to n
-// #include <stdio.h>
-// int main(){
-//     int n, s = 0;
-//     scanf("%d", &n);
-//     for(int i = 1; i <= n; i++){
-//         s = s = i;
-//         printf("%d\n", s);
-//     }
-//     return 0;
-// }
+// Print 1 to n
+#include <stdio.h>
+int main(){
+    int n, s = 0;
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++){
+        s = s = i;
+        printf("%d\n", s);
+    }
+    return 0;
+}
