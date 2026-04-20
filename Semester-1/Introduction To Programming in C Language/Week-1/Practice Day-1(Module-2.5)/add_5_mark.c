18f7356089eff6f439fa9fb3544065b0ac76b4e7
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-20 16:23:45 +0600

    feat(Semester-1/Week-1/Practice-Day-1): add program to increase input number by 5

--- Semester-1/Introduction To Programming in C Language/Week-1/Practice Day-1(Module-2.5)/add_5_mark.c
+++ Semester-1/Introduction To Programming in C Language/Week-1/Practice Day-1(Module-2.5)/add_5_mark.c
@@ -1,8 +1,8 @@
-// #include <stdio.h>
-// int main() {
-//     int b = 5, n, sum;
-//     scanf("%d", &n);
-//     sum = b + n;
-//     printf("%d", sum);
-//     return 0;
-// }
+#include <stdio.h>
+int main() {
+    int b = 5, n, sum;
+    scanf("%d", &n);
+    sum = b + n;
+    printf("%d", sum);
+    return 0;
+}
