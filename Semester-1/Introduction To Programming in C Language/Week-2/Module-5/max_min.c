e169b0dad4d1d0e4707b01b5c48ae4b0ca2ce76f
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-22 23:21:25 +0600

    feat(Semester-1/Week-2/Module-5): add program to find minimum and maximum among three integers

--- Semester-1/Introduction To Programming in C Language/Week-2/Module-5/max_min.c
+++ Semester-1/Introduction To Programming in C Language/Week-2/Module-5/max_min.c
@@ -1,48 +1,48 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int a, b, c;
-//     scanf("%d %d %d", &a, &b, &c);
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
 
-//     // min
-//     if(a <= b && a <= c) {
-//         printf("%d", a);
-//     } else if (b <= a && b <= c)
-//     {
-//         printf("%d", b);
-//     } else if (c <= a && c <= b) {
-//         printf("%d  ", c);
-//     }
+    // min
+    if(a <= b && a <= c) {
+        printf("%d", a);
+    } else if (b <= a && b <= c)
+    {
+        printf("%d", b);
+    } else if (c <= a && c <= b) {
+        printf("%d  ", c);
+    }
 
-//     //mix
-//     if(a >= b && a >= c) {
-//         printf("%d", a);
-//     } else if (b >= a && b >= c)
-//     {
-//         printf("%d", b);
-//     } else if (c >= a && c >= b) {
-//         printf("%d", c);
-//     }
+    //mix
+    if(a >= b && a >= c) {
+        printf("%d", a);
+    } else if (b >= a && b >= c)
+    {
+        printf("%d", b);
+    } else if (c >= a && c >= b) {
+        printf("%d", c);
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int a, b, c;
-//     scanf("%d %d %d", &a, &b, &c);
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
 
-//     int min = a, max = a;
+    int min = a, max = a;
 
-//     if (b < min) min = b;
-//     if (c < min) min = c;
+    if (b < min) min = b;
+    if (c < min) min = c;
 
-//     if (b > max) max = b;
-//     if (c > max) max = c;
+    if (b > max) max = b;
+    if (c > max) max = c;
 
-//     printf("%d %d\n", min, max);
+    printf("%d %d\n", min, max);
 
-//     return 0;
-// }
+    return 0;
+}
