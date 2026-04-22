11f813e91ad53bb28f2fe2270f32d835abcb8211
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-22 23:05:10 +0600

    feat(Semester-1/Week-1/Practice-Day-2): add practice programs (print message, sum of two numbers, variables input/output, divisible by 5 check)

---
+++ Semester-1/Introduction To Programming in C Language/Week-3/Module-10/String_input_with_space.c
@@ -0,0 +1,24 @@
+// #include <stdio.h>
+// #include <string.h> // gets(), fgets()
+// int main()
+// {
+//     char a[50];
+//     // scanf("%s", &a);  // always eta use korbo. string er spage thakle -> fgets() use korbo. 
+//     // gets(a);          // kichu kichu devise e kaj kore na
+//     fgets(a, 50, stdin); // fgets = standard, sob device e kaj kore
+
+//     printf("%s", a);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     char str[20];
+//     scanf("%c", &str);
+//     printf("%s", str);
+//     return 0;
+// }
+
+
+// fgets() -> eitar size e Null soho neoyar jonno 1 bariye nite hobe
