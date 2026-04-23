4948716f6314854c83495adb7c723225b882e4ef
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-24 01:07:23 +0600

    feat(Assignment-3/Count_Me_3): add program to count capital letters, small letters and digits in string

--- All Assignment/Assignment-3/Count_Me_3.c
+++ All Assignment/Assignment-3/Count_Me_3.c
@@ -1,28 +1,30 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int T;
-//     scanf("%d", &T);
+int main() {
+    int T;
+    scanf("%d", &T);
 
-//     for (int i = 0; i < T; i++){
-//         char s[10001];
-//         scanf("%s", s);
+    for (int i = 0; i < T; i++){
+        char s[10001];
+        scanf("%s", s);
 
-//         int capital = 0, small = 0, digits = 0;
-//         for (int i = 0; s[i] != '\0'; i++){
-//             if (s[i] >= 'A' && s[i] <= 'Z') {
-//                 capital++;
-//             }
-//             if (s[i] >= 'a' && s[i] <= 'z') {
-//                 small++;
-//             }
-//             if (s[i] >= '0' && s[i] <= '9') {
-//                 digits++;
-//             }
-//         }
-//         printf("%d %d %d\n", capital, small, digits);
-//     }
+        int capital = 0, small = 0, digits = 0;
+        for (int i = 0; s[i] != '\0'; i++){
+            if (s[i] >= 'A' && s[i] <= 'Z') {
+                capital++;
+            }
+            if (s[i] >= 'a' && s[i] <= 'z') {
+                small++;
+            }
+            if (s[i] >= '0' && s[i] <= '9') {
+                digits++;
+            }
+        }
+        printf("%d %d %d\n", capital, small, digits);
+    }
 
-//     return 0;
-// }
+    return 0;
+}
+
+
 
