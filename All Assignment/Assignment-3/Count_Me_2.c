53bf08bf0111b7fe2e7cf653bcd753512ed0e0a9
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-24 01:02:22 +0600

    feat(Assignment-3/Count_Me_2): add program to count consonants in string

--- All Assignment/Assignment-3/Count_Me_2.c
+++ All Assignment/Assignment-3/Count_Me_2.c
@@ -1,17 +1,17 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     char S[100001];
-//     scanf("%s", &S);
+int main() {
+    char S[100001];
+    scanf("%s", &S);
 
-//     int count = 0;
-//     for (int i = 0; S[i] != '\0' ; i++){
-//         if(S[i] != 'a' && S[i] != 'e' && S[i] != 'i' && S[i] != 'o' && S[i] != 'u' ){
-//             count++;
-//         }
-//     }
+    int count = 0;
+    for (int i = 0; S[i] != '\0' ; i++){
+        if(S[i] != 'a' && S[i] != 'e' && S[i] != 'i' && S[i] != 'o' && S[i] != 'u' ){
+            count++;
+        }
+    }
 
-//     printf("%d", count);
-//     return 0;
-// }
+    printf("%d", count);
+    return 0;
+}
 
