61d6844a11df55f5994f80b2402e8d6861d8b5d2
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-26 22:45:13 +0600

    feat(Catch-Up-Week/C_Simple_Calculator): add program to perform addition, multiplication, and subtraction of two integers

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/C_Simple_Calculator.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/C_Simple_Calculator.c
@@ -1,15 +1,15 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int X, Y, sum, sub;
-//     long long int mul;
-//     scanf("%d %d", &X, &Y);
-//     sum = X + Y;
-//     mul = (long long)X * Y;
-//     sub = X - Y;
+int main() {
+    int X, Y, sum, sub;
+    long long int mul;
+    scanf("%d %d", &X, &Y);
+    sum = X + Y;
+    mul = (long long)X * Y;
+    sub = X - Y;
 
-//     printf("%d + %d = %d\n",X, Y, sum);
-//     printf("%d * %d = %lld\n",X, Y, mul);
-//     printf("%d - %d = %d\n",X, Y, sub);
-//     return 0;
-// }
+    printf("%d + %d = %d\n",X, Y, sum);
+    printf("%d * %d = %lld\n",X, Y, mul);
+    printf("%d - %d = %d\n",X, Y, sub);
+    return 0;
+}
