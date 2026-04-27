c50de920019ba94af29007df9de842d6a147125f
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-28 02:16:23 +0600

    feat(Catch-Up-Week/H_Two_numbers): add solution to perform division and show floor, ceil, and round results

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/H_Two_numbers.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/H_Two_numbers.c
@@ -1,6 +1,13 @@
-// #include <stdio.h>
+#include <stdio.h>
+#include <math.h>
 
-// int main() {
-    
-//     return 0;
-// }
+int main() {
+    double a, b, div = 0;
+    scanf("%lf %lf", &a, &b);
+    div = a/b;
+
+    printf("floor %.0lf / %.0lf = %.0lf\n", a, b, floor(div));
+    printf("ceil %.0lf / %.0lf = %.0lf\n", a, b, ceil(div));
+    printf("round %.0lf / %.0lf = %.0lf\n", a, b, round(div));
+    return 0;
+}
---
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/L_The_Brothers.c
@@ -0,0 +1,12 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+//     char f1[1000], s1[1000], f2[1000], s2[1000];
+//     scanf("%s %s", f1, s1);
+//     scanf("%s %s", f2, s2);
+
+//     if (strcasecmp(s1, s2) == 0) printf("ARE Brothers");
+//     else printf("NOT");
+//     return 0;
+// }
