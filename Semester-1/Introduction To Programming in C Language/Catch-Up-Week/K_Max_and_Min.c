e8c8f977bb2dfd3f1583f9ae7089452bb3c96fa9
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-28 02:19:55 +0600

    feat(Catch-Up-Week/K_Max_and_Min): add solution to find minimum and maximum among three integers

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/K_Max_and_Min.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/K_Max_and_Min.c
@@ -1,16 +1,17 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int a, b, c, min, max;
-//     scanf("%d %d %d", &a, &b, &c);
-//     min = a;
-//     max = a;
+int main() {
+    int a, b, c, min, max;
+    scanf("%d %d %d", &a, &b, &c);
+    min = a;
+    max = a;
 
-//     if(b < min) min = b;
-//     if(c < min) min = c;
-//     if(b > max) max = b;
-//     if(c > max) max = c;
+    if(b < min) min = b;
+    if(c < min) min = c;
+    if(b > max) max = b;
+    if(c > max) max = c;
 
-//     printf("%d %d", min, max);
-//     return 0;
-// }
+    printf("%d %d", min, max);
+    return 0;
+}
+
