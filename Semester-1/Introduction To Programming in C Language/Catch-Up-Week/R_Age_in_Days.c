a7ecf97922c9cb525f3aada17e2faaef809ed7cb
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-29 03:25:29 +0600

    feat(Catch-Up-Week/R_Age_in_Days): add solution to convert total days into years, months, and days

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/R_Age_in_Days.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/R_Age_in_Days.c
@@ -1,16 +1,16 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int n, years, months, days;
-//     scanf("%d", &n);
+int main() {
+    int n, years, months, days;
+    scanf("%d", &n);
 
-//     years = n / 365;
-//     months = (n % 365) / 30;
-//     days = (n % 365) % 30;
+    years = n / 365;
+    months = (n % 365) / 30;
+    days = (n % 365) % 30;
 
-//     printf("%d years\n", years);
-//     printf("%d months\n", months);
-//     printf("%d days\n", days);
-//     return 0;
-// }
+    printf("%d years\n", years);
+    printf("%d months\n", months);
+    printf("%d days\n", days);
+    return 0;
+}
 
