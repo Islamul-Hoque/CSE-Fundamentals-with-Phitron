5bc9e091e85101dd834ca4f924cc8dd26365198c
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-29 03:23:20 +0600

    feat(Catch-Up-Week/P_First_digit): add solution to check if the thousands digit of a number is even or odd

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/P_First_digit.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/P_First_digit.c
@@ -1,12 +1,12 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int n;
-//     scanf("%d", &n);
+int main() {
+    int n;
+    scanf("%d", &n);
 
-//     if((n/1000) % 2 == 0 ) printf("EVEN");
-//     else printf("ODD");
-//     return 0;
-// }
+    if((n/1000) % 2 == 0 ) printf("EVEN");
+    else printf("ODD");
+    return 0;
+}
 
 
