e4d6aa45d75e89ece86adb58caf1d61e12a87b35
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-29 03:26:42 +0600

    feat(Catch-Up-Week/T_Sort_Numbers): add solution to sort three integers and identify min, mid, and max

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/T_Sort_Numbers.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/T_Sort_Numbers.c
@@ -1,25 +1,26 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int a, b, c, min = 0, mid = 0, max = 0;
-//     scanf("%d %d %d", &a, &b, &c);
+int main() {
+    int a, b, c, min = 0, mid = 0, max = 0;
+    scanf("%d %d %d", &a, &b, &c);
 
-//     min = a;
-//     max = a;
+    min = a;
+    max = a;
 
-//     // min
-//     if (b <= min) min = b;
-//     if (c <= min) min = c;
+    // min
+    if (b <= min) min = b;
+    if (c <= min) min = c;
 
-//     // max
-//     if (b >= max) max = b;
-//     if (c >= max) max = c;
+    // max
+    if (b >= max) max = b;
+    if (c >= max) max = c;
 
-//     // middle
-//     mid = a + b + c - min - max;
+    // middle
+    mid = a + b + c - min - max;
 
-//     printf("%d\n%d\n%d\n\n", min, mid, max);
-//     printf("%d\n%d\n%d", a, b, c);
-//     return 0;
-// }
+    printf("%d\n%d\n%d\n\n", min, mid, max);
+    printf("%d\n%d\n%d", a, b, c);
+    return 0;
+}
+
 
