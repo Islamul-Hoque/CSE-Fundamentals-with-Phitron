a2ab888c2b12b25fcf3a01d8c6b7bf2f90d25be0
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-29 03:24:35 +0600

    feat(Catch-Up-Week/Q_Coordinates_of_a_Point): add solution to determine point position in coordinate system (origin, axis, or quadrant)

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Q_Coordinates_of_a_Point.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Q_Coordinates_of_a_Point.c
@@ -1,17 +1,17 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     float x, y;
-//     scanf("%f %f", &x, &y);
+int main() {
+    float x, y;
+    scanf("%f %f", &x, &y);
 
-//     if( x == 0 && y == 0 ) printf("Origem");
-//     else if( x == 0 ) printf("Eixo Y");
-//     else if( y == 0 ) printf("Eixo X");
+    if( x == 0 && y == 0 ) printf("Origem");
+    else if( x == 0 ) printf("Eixo Y");
+    else if( y == 0 ) printf("Eixo X");
 
-//     else if( x >0 && y >0 ) printf("Q1");
-//     else if( x <0 && y >0 ) printf("Q2");
-//     else if( x <0 && y <0 ) printf("Q3");
-//     else if( x >0 && y <0 ) printf("Q4");
-//     return 0;
-// }
+    else if( x >0 && y >0 ) printf("Q1");
+    else if( x <0 && y >0 ) printf("Q2");
+    else if( x <0 && y <0 ) printf("Q3");
+    else if( x >0 && y <0 ) printf("Q4");
+    return 0;
+}
 
