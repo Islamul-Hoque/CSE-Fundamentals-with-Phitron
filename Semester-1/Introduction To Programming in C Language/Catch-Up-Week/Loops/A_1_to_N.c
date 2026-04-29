e8c0099620a3057826f390432271e9f476da15ac
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-30 00:27:33 +0600

    feat(Loops/A_1_to_N): add solution to print numbers from 1 to N using for loop

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Loops/A_1_to_N.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Loops/A_1_to_N.c
@@ -1,10 +1,10 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int n;
-//     scanf("%d", &n);
+int main() {
+    int n;
+    scanf("%d", &n);
 
-//     for (int i = 1; i <= n; i++) printf("%d\n", i);
+    for (int i = 1; i <= n; i++) printf("%d\n", i);
 
-//     return 0;
-// }
+    return 0;
+}
