d1a9380b69491528a9551ed0c97252c002bc7c88
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-04-30 00:23:28 +0600

    feat(Data-Type-Conditions/Z_Hard_Compare): add solution to compare a^b and c^d using logarithmic properties to avoid overflow

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Data-Type-Conditions/Z_Hard_Compare.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Data-Type-Conditions/Z_Hard_Compare.c
@@ -1,12 +1,13 @@
-// #include <stdio.h>
-// #include <math.h>
+#include <stdio.h>
+#include <math.h>
 
-// int main() {
-//     long long int a, b, c, d;
-//     scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+int main() {
+    long long int a, b, c, d;
+    scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
 
-//     if(b*log(a) > d*log(c)) printf("YES");
-//     else printf("NO");
+    if(b*log(a) > d*log(c)) printf("YES");
+    else printf("NO");
 
-//     return 0;
-// }
+    return 0;
+}
+
