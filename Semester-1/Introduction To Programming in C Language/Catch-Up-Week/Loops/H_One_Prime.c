50c74c8dacd2ae847f1ea5f638dc6a21d3a967d0
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-01 23:50:17 +0600

    feat(Loops/F_Multiplication_table): add solution to generate multiplication table of a number from 1 to 12 using for loop

---
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Loops/H_One_Prime.c
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+// #include <math.h>
+
+// int main() {
+//     int n, isPrime=1;
+//     scanf("%d", &n);
+
+//     for( int i = 2; i <= sqrt(n); i++){
+//         if(n % i == 0) {
+//             isPrime = 0;
+//             break;
+//         }
+//     }
+
+//     if(isPrime == 1) printf("YES\n");
+//     else printf("NO\n");
+
+//     return 0;
+// }
