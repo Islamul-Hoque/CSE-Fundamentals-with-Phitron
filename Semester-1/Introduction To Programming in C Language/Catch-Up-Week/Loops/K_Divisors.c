bc74c33e9316a40c00a57af9352e9f03c6595666
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-01 23:56:37 +0600

    feat(Loops/K_Divisors): add solution to print all divisors of a number using for loop

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Loops/K_Divisors.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Loops/K_Divisors.c
@@ -1,11 +1,11 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int n;
-//     scanf("%d", &n);
+int main() {
+    int n;
+    scanf("%d", &n);
 
-//     for(int i=1; i<=n; i++)
-//         if(n % i == 0) printf("%d\n", i);
+    for(int i=1; i<=n; i++)
+        if(n % i == 0) printf("%d\n", i);
 
-//     return 0;
-// }
+    return 0;
+}
