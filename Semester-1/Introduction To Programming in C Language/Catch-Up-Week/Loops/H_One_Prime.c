70a3a5da02e31575df89ef14c213661308bf3fc9
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-01 23:52:12 +0600

    feat(Functions/Loops/H_One_Prime): add solution to check if a number is prime using loop and sqrt optimization

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Loops/H_One_Prime.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Loops/H_One_Prime.c
@@ -1,19 +1,19 @@
-// #include <stdio.h>
-// #include <math.h>
+#include <stdio.h>
+#include <math.h>
 
-// int main() {
-//     int n, isPrime=1;
-//     scanf("%d", &n);
+int main() {
+    int n, isPrime=1;
+    scanf("%d", &n);
 
-//     for( int i = 2; i <= sqrt(n); i++){
-//         if(n % i == 0) {
-//             isPrime = 0;
-//             break;
-//         }
-//     }
+    for( int i = 2; i <= sqrt(n); i++){
+        if(n % i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
 
-//     if(isPrime == 1) printf("YES\n");
-//     else printf("NO\n");
+    if(isPrime == 1) printf("YES\n");
+    else printf("NO\n");
 
-//     return 0;
-// }
+    return 0;
+}
