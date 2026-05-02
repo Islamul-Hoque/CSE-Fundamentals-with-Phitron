8d3d786155261ea6f6226af01cd72ca4480ddd19
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-03 01:37:46 +0600

    feat(Module-10/String_initialization): add solution to demonstrate string initialization and printing

--- Semester-1/Introduction To Programming in C Language/Week-3/Module-10/String_initialization.c
+++ Semester-1/Introduction To Programming in C Language/Week-3/Module-10/String_initialization.c
@@ -1,11 +1,11 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main()
-// {
-//     // char s[5] = {'a', 'b', 'c', 'd'};
-//     char s[50] = "Islamul Hoque";
-//     printf("%s", s);
-//     return 0;
-// }
+int main()
+{
+    // char s[5] = {'a', 'b', 'c', 'd'};
+    char s[50] = "Islamul Hoque";
+    printf("%s", s);
+    return 0;
+}
 
 
