b977b10a835c941a4e5390644d034c1dd6c73513
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-03 01:15:13 +0600

    feat(Module-10/B_Let_s_use_Getline): add solution to read and print string using fgets and loop

--- Semester-1/Introduction To Programming in C Language/Week-3/Module-10/B_Let_s_use_Getline.c
+++ Semester-1/Introduction To Programming in C Language/Week-3/Module-10/B_Let_s_use_Getline.c
@@ -1,10 +1,11 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     char s[1000001];
-//     fgets(s, 1000001, stdin);
+int main() {
+    char s[1000001];
+    fgets(s, 1000001, stdin);
 
-//     for(int i = 0; s[i] != '\\'; i++) printf("%c", s[i]);
+    for(int i = 0; s[i] != '\\'; i++) printf("%c", s[i]);
 
-//     return 0;
-// }
+    return 0;
+}
+
