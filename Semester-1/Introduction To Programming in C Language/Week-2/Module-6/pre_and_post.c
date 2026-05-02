2c889c510be459762bc712d993ab86a2760bc1d0
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-02 23:58:10 +0600

    feat(Module-6/pre_and_post): add solution to demonstrate pre-increment and post-increment operator behavior

--- Semester-1/Introduction To Programming in C Language/Week-2/Module-6/pre_and_post.c
+++ Semester-1/Introduction To Programming in C Language/Week-2/Module-6/pre_and_post.c
@@ -1,10 +1,10 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int x = 10, y, z; 
-//     y = x++; 
-//     z = ++y; 
+int main() {
+    int x = 10, y, z; 
+    y = x++; 
+    z = ++y; 
 
-//     printf("%d", z ++);
-//     return 0;
-// }
+    printf("%d", z ++);
+    return 0;
+}
