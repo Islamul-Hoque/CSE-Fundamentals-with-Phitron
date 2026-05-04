8b9c693c40a35cea8f881ab0b4aa10309f6309e0
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-04 16:58:43 +0600

    feat(Module-11/String_copy): add solution to copy strings manually using loop and built-in strcpy function

--- Semester-1/Introduction To Programming in C Language/Week-3/Module-11/String_copy.c
+++ Semester-1/Introduction To Programming in C Language/Week-3/Module-11/String_copy.c
@@ -1,15 +1,17 @@
-// #include <stdio.h>
-// #include <string.h>
-// int main()
-// {
-//     char a[101], b[101];
-//     scanf("%s %s", &a, &b);
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char a[101], b[101];
+    scanf("%s %s", &a, &b);
 
-//     // int length = strlen(b);
-//     // for (int i = 0; i <=length; i++) a[i] = b[i]; // null soho print korte hobe.
+    // manually string copy 
+    int length = strlen(b);
+    for (int i = 0; i <=length; i++) a[i] = b[i]; // null soho print korte hobe.
 
-//     strcpy(a, b);
-//     printf("%s %s", a, b);
-//     return 0;
-// }
+    // build-in strcpy() function
+    strcpy(a, b);
+    printf("%s %s", a, b);
+    return 0;
+}
 
