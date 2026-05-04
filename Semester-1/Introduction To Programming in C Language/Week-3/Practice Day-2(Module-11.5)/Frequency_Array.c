439029aa82249ae8028ba6c0a02d47c9c9762539
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-04 17:06:59 +0600

    feat(Module-11.5/Frequency_Array): add solution to count frequency of integers (0-5) in an array

--- Semester-1/Introduction To Programming in C Language/Week-3/Practice Day-2(Module-11.5)/Frequency_Array.c
+++ Semester-1/Introduction To Programming in C Language/Week-3/Practice Day-2(Module-11.5)/Frequency_Array.c
@@ -1,24 +1,24 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main()
-// {
-//     int n;
-//     scanf("%d",&n);
-//     int a[n];
-//     for(int i=0;i<n;i++)
-//     {
-//         scanf("%d",&a[i]);
-//     }
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
 
-//     int fre[6] = {0}; // 
+    int fre[6] = {0}; // 
 
-//     for(int i=0;i<n;i++) {
-//         int val = a[i];
-//         fre[val]++;
-//     }
+    for(int i=0;i<n;i++) {
+        int val = a[i];
+        fre[val]++;
+    }
 
-//     for(int i=0;i<6;i++) {
-//         printf("%d -> %d\n",i,fre[i]);
-//     }
-//     return 0;
-// }
+    for(int i=0;i<6;i++) {
+        printf("%d -> %d\n",i,fre[i]);
+    }
+    return 0;
+}
