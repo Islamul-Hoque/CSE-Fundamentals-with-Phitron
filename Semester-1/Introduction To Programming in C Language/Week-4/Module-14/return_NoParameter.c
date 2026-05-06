3dd4e3da0c25175c00898e4da01a477dd3008275
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-06 15:45:56 +0600

    feat(Module-14/return_NoParameter): add implementation of function returning value without parameters (sum function)

--- Semester-1/Introduction To Programming in C Language/Week-4/Module-14/return_NoParameter.c
+++ Semester-1/Introduction To Programming in C Language/Week-4/Module-14/return_NoParameter.c
@@ -1,17 +1,16 @@
-// #include <stdio.h>
+#include <stdio.h>
 
 
-// int sum(){
-//     int num1, num2;
-//     scanf("%d %d", &num1, &num2);
-//     int ans = num1 + num2;
-//     return ans;
-// }
+int sum(){
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    int ans = num1 + num2;
+    return ans;
+}
 
-// int main()
-// {
-//     int result = sum();
-//     printf("Your sum is: %d", result);
+int main(){
+    int result = sum();
+    printf("Your sum is: %d", result);
 
-//     return 0;
-// }
+    return 0;
+}
