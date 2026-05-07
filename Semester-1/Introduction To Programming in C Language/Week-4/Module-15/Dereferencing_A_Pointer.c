56db9177961eba9d2ce31e6cf37cb5fe96e401b6
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-08 00:16:19 +0600

    feat(Module-15/Dereferencing_A_Pointer): add demonstration of pointer referencing and dereferencing with value update

--- Semester-1/Introduction To Programming in C Language/Week-4/Module-15/Dereferencing_A_Pointer.c
+++ Semester-1/Introduction To Programming in C Language/Week-4/Module-15/Dereferencing_A_Pointer.c
@@ -1,2 +1,18 @@
 // Dereferencing a pointer
-    printf("%d\n", *ptr);
+#include <stdio.h>
+
+int main() {
+    int x;
+    scanf("%d", &x); // taking input by referencing variable address
+
+    printf("%d\n", x);
+    printf("%p\n\n", &x);
+
+    int *p = &x; // Pointer referencing x
+    *p = 200;    // Dereferencing: update value via pointer
+    printf("%d\n", x);   // 200
+    printf("%d\n", *p);  // 200
+    printf("%p\n", &p);
+
+    return 0;
+}
