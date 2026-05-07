e387cf8ff91f0dc5e44c247ccc4c99b5aaf8acc3
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-07 18:15:30 +0600

    feat(Module-14/Useful_Math_Functions): add demonstration of standard math functions (ceil, floor, round, sqrt, pow, abs)

---
+++ Semester-1/Introduction To Programming in C Language/Week-4/Module-14/Useful_Math_Functions.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double n = 5.3;
+    printf("Original number: %.2f\n", n);
+    printf("Ceil value     : %.0f\n", ceil(n));
+    printf("Floor value    : %.0f\n", floor(n));
+
+    printf("Round value    : %.0f\n\n", round(n)); // 5
+
+    double m = 5.5;
+    printf("Original number: %.2f\n", m);
+    printf("Round value    : %.0f\n\n", round(m)); // 6
+
+    double p = 64.5;
+    printf("Number: %.2lf\n", p);
+    printf("Square root of %.2lf = %.2lf\n\n", p, sqrt(p));
+
+    printf("Result of pow is = %.0f\n\n", pow(2, 3));
+
+    printf("abs(-10) = %d\n", abs(-10));
+    printf("abs(10)  = %d\n", abs(10));
+    printf("abs(0)   = %d\n", abs(0));
+    return 0;
+}
