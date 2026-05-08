2145ffe9d0c669983a5310e776bebeba53c35327
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-08 22:03:00 +0600

    feat(Assignment-3/Even_and_Odd): add no-parameter no-return function to count even and odd numbers and print result

---
+++ All Assignment/Assignment-3/Even_and_Odd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void odd_even(){
+    int n, odd=0, even=0;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i <n; i++){
+        scanf("%d", &a[i]);
+        if(a[i] % 2 == 0){
+            even++;
+        } else odd++;
+    }
+    printf("%d %d", even, odd);
+}
+
+int main() {
+    odd_even();
+
+    return 0;
+}
