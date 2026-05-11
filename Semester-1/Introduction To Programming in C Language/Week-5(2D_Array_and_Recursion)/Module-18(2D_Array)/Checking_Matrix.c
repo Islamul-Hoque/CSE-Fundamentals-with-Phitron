7d962830ca467f74626dc2d9be4f91b61582716a
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-11 20:03:24 +0600

    feat(Module-18/Checking_Matrix): add solution to identify row, column, or square matrix based on dimensions

---
+++ Semester-1/Introduction To Programming in C Language/Week-5(2D_Array_and_Recursion)/Module-18(2D_Array)/Checking_Matrix.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int a[r][c];
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+
+    if (r == 1)
+        printf("This is a row Matrix.\n");
+    if (c == 1)
+        printf("This is a col Matrix.\n");
+    if (r == c)
+        printf("This is a square Matrix.\n");
+    
+    return 0;
+}
+
