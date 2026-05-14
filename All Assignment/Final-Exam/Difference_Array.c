edee3c527c5d81c5102499a2d81c37ea5f446bea
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-14 14:59:14 +0600

    feat(Final-Exam/Difference_Array): implement algorithm to copy array, sort ascending, and compute absolute difference with original array across multiple test cases

--- All Assignment/Final-Exam/Difference_Array.c
+++ All Assignment/Final-Exam/Difference_Array.c
@@ -1,39 +1,35 @@
-// #include <stdio.h>
-// #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// int main() {
-//     int t,n;
-//     scanf("%d", &t);
-    
-//     while(t--){
-//         scanf("%d", &n);
-//         int a[n];
-//         for (int i = 0; i <n; i++) {
-//             scanf("%d", &a[i]);
-//         }
+int main() {
+    int t,n;
+    scanf("%d", &t);
 
-//         int b[n];
-//         for (int i = 0; i <n; i++) { // copy a in b
-//             b[i] = a[i];
-//         }
+    while(t--){
+        scanf("%d", &n);
+        int a[n];
+        for (int i = 0; i <n; i++) scanf("%d", &a[i]);
 
-//         for (int i = 0; i <n; i++) { 
-//             for (int j = i+1; j <n; j++) { // ascending order
-//                 if(b[i] > b[j]){
-//                     int temp = b[i];
-//                     b[i] = b[j];
-//                     b[j] = temp;
-//                 }
-//             }
-//         }
+        int b[n];
+        for (int i = 0; i <n; i++) b[i] = a[i]; // copy a in b
 
-//         int c[n];
-//         for (int i = 0; i <n; i++) {  // absolute difference
-//             c[i] = abs(b[i] - a[i]);
-//             printf("%d ", c[i]);
-//         }
-//         printf("\n");
-//     }
+        for (int i = 0; i <n; i++) { 
+            for (int j = i+1; j <n; j++) { // ascending order
+                if(b[i] > b[j]){
+                    int temp = b[i];
+                    b[i] = b[j];
+                    b[j] = temp;
+                }
+            }
+        }
 
-//     return 0;
-// }
+        int c[n];
+        for (int i = 0; i <n; i++) {  // absolute difference
+            c[i] = abs(b[i] - a[i]);
+            printf("%d ", c[i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
