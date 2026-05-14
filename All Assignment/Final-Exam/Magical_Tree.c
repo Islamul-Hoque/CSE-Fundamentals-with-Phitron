7e0050a6c128ca607a08d56f8e19db96974015d3
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-14 14:44:00 +0600

    feat(Final-Exam/Magical_Tree): implement algorithm to print Christmas tree pattern (pyramid stars with trunk) based on input size N

--- All Assignment/Final-Exam/Magical_Tree.c
+++ All Assignment/Final-Exam/Magical_Tree.c
@@ -1,38 +1,33 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int n;
-//     scanf("%d", &n);
-//     int total_Line = 5 + (n + 1) / 2;
-//     int space=total_Line-1, star=1;
+int main() {
+    int n;
+    scanf("%d", &n);
 
-//     for (int i = 1; i <=total_Line; i++) { // for printing line
-//         for (int k = 1; k <=space; k++) { // for printing space
-//             printf(" ");
-//         }
-//         for (int j = 1; j <=star; j++) { // for printing space
-//             printf("*");
-//         }
+    int total_Line = 5 + (n + 1) / 2;
+    int space=total_Line-1, star=1;
 
-//         space--;
-//         star+=2;
-//         printf("\n");
-//     }
+    for (int i = 1; i <=total_Line; i++) {  // for printing line
+        for (int k = 1; k <=space; k++)  printf(" ");  // for printing space
+        for (int j = 1; j <=star; j++) printf("*");    // for printing space
 
-//     for (int i = 0; i <5; i++) { // for printing line
-//         for (int k = 0; k <5; k++) { // for printing space
-//             printf(" ");
-//         }
-//         for (int j = 1; j <=n; j++) { // for printing star
-//             printf("*");
-//         }
-//         printf("\n");
-//     }
+        space--;
+        star+=2;
+        printf("\n");
+    }
 
-//     return 0;
-// }
+    for (int i = 0; i <5; i++) {    // for printing line
+        for (int k = 0; k <5; k++) printf(" ");    // for printing space
+        for (int j = 1; j <=n; j++)  printf("*");  // for printing star
+
+        printf("\n");
+    }
+
+    return 0;
+}
 
 
+// For Ster
 // n=1-> 6 line
 // n=3-> 7 line
 // n=5-> 8line
@@ -40,31 +35,7 @@
 // n=2 barle => 1ta kore line barteche
 
 
-
-// #include <stdio.h>
-
-// int main() {
-//     int n;
-//     scanf("%d", &n);
-
-//     int total_Line = 5 + (n + 1) / 2;
-//     int space=total_Line-1, star=1;
-
-//     for (int i = 1; i <=total_Line; i++) { 
-//         for (int k = 1; k <=space; k++)  printf(" ");
-//         for (int j = 1; j <=star; j++) printf("*");
-
-//         space--;
-//         star+=2;
-//         printf("\n");
-//     }
-
-//     for (int i = 0; i <5; i++) { 
-//         for (int k = 0; k <5; k++) printf(" ");
-//         for (int j = 1; j <=n; j++)  printf("*");
-
-//         printf("\n");
-//     }
-
-//     return 0;
-// }
+// for trunk
+// space = 5 ta
+// line = 5 ta
+// star = n er man 
