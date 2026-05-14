f285e66d6d845e7b97a5da88a3653a09f1c21552
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-14 14:28:40 +0600

    feat(Final-Exam/Jadu_Matrix): implement validation to check if a matrix is Jadu Matrix (square matrix with 1s on both diagonals and 0s elsewhere) and print YES/NO accordingly

--- All Assignment/Final-Exam/Jadu_Matrix.c
+++ All Assignment/Final-Exam/Jadu_Matrix.c
@@ -1,31 +1,31 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main(){
-//     int r, c, flag=1;
-//     scanf("%d %d", &r, &c);
-//     int a[r][c];
+int main(){
+    int r, c, flag=1;
+    scanf("%d %d", &r, &c);
+    int a[r][c];
 
-//     for (int i = 0; i < r; i++)
-//         for (int j = 0; j < c; j++)
-//             scanf("%d", &a[i][j]);
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
 
-//     if(r==c){
-//         for (int i = 0; i < r; i++){
-//             for (int j = 0; j < c; j++){
-//                 if (i==j || i+j == r-1) {
-//                     if(a[i][j] != 1) flag=0;
-//                 } else {
-//                     if(a[i][j] != 0) flag=0;
-//                 }
-//             }
-//         }
-//     } else flag = 0;
+    if(r==c){
+        for (int i = 0; i < r; i++){
+            for (int j = 0; j < c; j++){
+                if (i==j || i+j == r-1) {
+                    if(a[i][j] != 1) flag=0;
+                } else {
+                    if(a[i][j] != 0) flag=0;
+                }
+            }
+        }
+    } else flag = 0;
 
-//     if(flag == 1){
-//         printf("YES");
-//     } else {
-//         printf("NO");
-//     }
+    if(flag == 1){
+        printf("YES");
+    } else {
+        printf("NO");
+    }
 
-//     return 0;
-// }
+    return 0;
+}
