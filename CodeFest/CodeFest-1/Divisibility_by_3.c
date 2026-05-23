9a04cf22141105662d3b540284dc972ef5947414
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-23 23:09:11 +0600

    feat(CodeFest-1/Divisibility-by-3): add program to check divisibility of a number by 3 using digit sum

--- CodeFest/CodeFest-1/Divisibility_by_3.c
+++ CodeFest/CodeFest-1/Divisibility_by_3.c
@@ -1,19 +1,19 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int n;  
-//     scanf("%d", &n);
+int main() {
+    int n;  
+    scanf("%d", &n);
 
-//     char num[n+1];
-//     scanf("%s", num);
+    char num[n+1];
+    scanf("%s", num);
 
-//     int digit_sum = 0;
-//     for (int i = 0; i<n; i++) {
-//         digit_sum += num[i] - '0'; 
-//     }
+    int digit_sum = 0;
+    for (int i = 0; i<n; i++) {
+        digit_sum += num[i] - '0'; 
+    }
 
-//     if (digit_sum % 3 == 0)    printf("YES");
-//     else printf("NO");
+    if (digit_sum % 3 == 0)    printf("YES");
+    else printf("NO");
 
-//     return 0;
-// }
+    return 0;
+}
