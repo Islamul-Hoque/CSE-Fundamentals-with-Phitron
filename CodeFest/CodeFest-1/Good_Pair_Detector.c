910b29ef3a93020626037e2977474854bee8968c
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-23 23:08:21 +0600

    feat(CodeFest-1/Good-Pair-Detector): add program to count good pairs by multiplying even and odd counts in an array

--- CodeFest/CodeFest-1/Good_Pair_Detector.c
+++ CodeFest/CodeFest-1/Good_Pair_Detector.c
@@ -1,18 +1,18 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main() {
-//     int n, even=0, odd=0;
-//     scanf("%d", &n);
-//     int a[n];
+int main() {
+    int n, even=0, odd=0;
+    scanf("%d", &n);
+    int a[n];
 
-//     for (int i = 0; i <n; i++) 
-//         scanf("%d", &a[i]);
+    for (int i = 0; i <n; i++) 
+        scanf("%d", &a[i]);
 
-//     for (int i = 0; i <n; i++){
-//         if(a[i] %2 == 0) even ++;
-//         else odd++;
-//     }
+    for (int i = 0; i <n; i++){
+        if(a[i] %2 == 0) even ++;
+        else odd++;
+    }
 
-//     printf("%d", even * odd);
-//     return 0;
-// }
+    printf("%d", even * odd);
+    return 0;
+}
