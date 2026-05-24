7a9a5cc9a222177e716db4a8c1c73a1158b90b1d
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-05-25 01:23:29 +0600

    feat(Module-2/Dynamic-Array-Return-From-Function): add program demonstrating dynamic array allocation and return from function

--- Semester-1/Introduction to C++ for DSA/Week-1/Module-2(Dynamic_Memory_Allocation)/Dynamic_Array.cpp
+++ Semester-1/Introduction to C++ for DSA/Week-1/Module-2(Dynamic_Memory_Allocation)/Dynamic_Array.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 
 int main() {
-    int *a = new int [5];
+    int a[5]; // Static Array
+    // int *a = new int [5]; // Dynamic Array
 
     for (int i = 0; i <5; i++)
         cin >> a[i];
