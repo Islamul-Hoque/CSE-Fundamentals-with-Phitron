669077a4504fb18b3596f0b66fd4c549b0984883
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-08 20:02:37 +0600

    feat(C++/Week-2/quiz.cpp): add multiple string practice programs covering append, push_back, empty, comparison, getline, cin.ignore, concatenation, and stringstream

--- Semester-1/Introduction to C++ for DSA/Week-2/Stringstream.cpp
+++ Semester-1/Introduction to C++ for DSA/Week-2/Stringstream.cpp
@@ -6,11 +6,16 @@ int main(){
     getline(cin, s);
     cout << s << endl;
 
-    // string কে word-by-word read kora
+    // string কে word-by-word read
     stringstream ss(s);
-    string word;  // main string ke word e assign korechi
-    while(ss >> word)// main string theke word niye oi word ta main string theke remove kore dibe.
-        cout << word << endl;
 
+    string word; 
+    int count =0;
+    while(ss>> word){ // প্রতিবার (ss >> word) করলে পরবর্তী word পাওয়া যায়
+            cout << word << endl;
+            count++;
+        }
+
+    cout << "Total word: " << count << endl;
     return 0;
 }
--- Semester-1/Introduction to C++ for DSA/Week-2/tempCodeRunnerFile.cpp
+++ Semester-1/Introduction to C++ for DSA/Week-2/tempCodeRunnerFile.cpp
@@ -1,5 +1,3 @@
-, 4);
-    cout << s << endl;
-
-    s.erase(3);
-    cout << s << endl;
+// Output:
+// Hello
+// World
