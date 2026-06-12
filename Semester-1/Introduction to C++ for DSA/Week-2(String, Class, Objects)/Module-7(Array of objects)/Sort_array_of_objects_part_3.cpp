05643b9ec809feb116b4afe5642d3b8f55fd199c
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-13 01:25:33 +0600

    feat(module-7/Sort_array_of_objects_part_3): implement sorting with tie-breaker logic using marks and roll (if-else, shortcut, ternary approaches)

--- Semester-1/Introduction to C++ for DSA/Week-2(String, Class, Objects)/Module-7(Array of objects)/Sort_array_of_objects_part_3.cpp
+++ Semester-1/Introduction to C++ for DSA/Week-2(String, Class, Objects)/Module-7(Array of objects)/Sort_array_of_objects_part_3.cpp
@@ -1,40 +1,51 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Student {
-//     public:
-//     string name;
-//     int roll;
-//     int marks;
-// };
+class Student {
+    public:
+    string name;
+    int roll;
+    int marks;
+};
 
-// bool cmp(Student l, Student r){
-//     if(l.marks > r.marks) 
-//         return true;
-//     else if(l.marks < r.marks) 
-//         return false;
+bool cmp(Student l, Student r){
+    // main process
+    if(l.marks > r.marks) 
+        return true;
+    else if(l.marks < r.marks) 
+        return false;
 
-//     // mark equal
-//     else { 
-//         if(l.roll < r.roll) 
-//             return true;
-//         else 
-//             return false;
-//     }
-// }
+    // mark equal
+    else { 
+        if(l.roll < r.roll) 
+            return true;
+        else 
+            return false;
+    }
 
-// int main(){
-//     int n;
-//     cin >> n;
-//     Student a[n];
+    // shortcut
+    // if else condition
+    if(l.marks == r.marks) 
+        return l.roll < r.roll;
+    else 
+        return l.marks > r.marks;
 
-//     for (int i = 0; i < n; i++) 
-//         cin >> a[i].name >> a[i].roll >> a[i].marks;
+    // ternary operator
+    return (l.marks == r.marks) ?  l.roll < r.roll : l.marks > r.marks;
+}
 
-//     sort(a, a+n, cmp);
+int main(){
+    int n;
+    cin >> n;
+    Student a[n];
 
-//     for (int i = 0; i < n; i++) 
-//         cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+    for (int i = 0; i < n; i++) 
+        cin >> a[i].name >> a[i].roll >> a[i].marks;
 
-//     return 0;
-// }
+    sort(a, a+n, cmp);
+
+    for (int i = 0; i < n; i++) 
+        cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+
+    return 0;
+}
