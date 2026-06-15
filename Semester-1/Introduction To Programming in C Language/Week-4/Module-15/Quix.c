b3aac87294f79d30fe56c9acde9132397459f749
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-15 18:31:52 +0600

    docs(readme/week-5/module-17): add recursion problem links including call stack, infinite recursion, print sequences (1→N, N→1, reverse, even), array printing, quiz, and extra practice problems

--- Semester-1/Introduction To Programming in C Language/Week-4/Module-15/Quix.c
+++ Semester-1/Introduction To Programming in C Language/Week-4/Module-15/Quix.c
@@ -1,54 +1,54 @@
-// // Q-1
-// #include <stdio.h>
+// Q-1
+#include <stdio.h>
 
-// void swap(int *p, int *q){
-//     int temp = *p;  // temp = *p অর্থাৎ temp = 6
-//     *p = *q;        // *p = *q অর্থাৎ a = 5
-//     *q = temp;      // *q = temp অর্থাৎ b = 6
-// }
+void swap(int *p, int *q){
+    int temp = *p;  // temp = *p অর্থাৎ temp = 6
+    *p = *q;        // *p = *q অর্থাৎ a = 5
+    *q = temp;      // *q = temp অর্থাৎ b = 6
+}
 
-// int main(){
-//     int a = 6, b = 5;
-//     swap(&a, &b);
-//     printf("%d %d\n", a, b);
-// }
-
-
-// // Q-2
-// #include <stdio.h>
-// void m(int *p){
-//     int i = 0;
-//     for (i = 0; i < 5; i++)
-//         printf("%d ", p[i]);
-// }
-
-// int main(){
-//     int a[5] = {6, 5, 3};
-//     m(a);
-// }
+int main(){
+    int a = 6, b = 5;
+    swap(&a, &b);
+    printf("%d %d\n", a, b);
+}
 
 
-// // Q-3
-// #include <stdio.h>
-// void m(int p, int q){
-//     int temp = p;
-//     p = q;
-//     q = temp;
-// }
+// Q-2
+#include <stdio.h>
+void m(int *p){
+    int i = 0;
+    for (i = 0; i < 5; i++)
+        printf("%d ", p[i]);
+}
 
-// int main(){
-//     int a = 6, b = 5;
-//     m(a, b);
-//     printf("%d %d\n", a, b);
-// }
+int main(){
+    int a[5] = {6, 5, 3};
+    m(a);
+}
 
 
-// // Q-4
-// #include <stdio.h>
-// int main() {
-//     int ary[4] = {1, 2, 3, 4};
-//     printf("%d\n", *ary); // *ary => 0 index er value 
-// } 
+// Q-3
+#include <stdio.h>
+void m(int p, int q){
+    int temp = p;
+    p = q;
+    q = temp;
+}
+
+int main(){
+    int a = 6, b = 5;
+    m(a, b);
+    printf("%d %d\n", a, b);
+}
+
+
+// Q-4
+#include <stdio.h>
+int main() {
+    int ary[4] = {1, 2, 3, 4};
+    printf("%d\n", *ary); // *ary => 0 index er value 
+} 
 
 
 // Q-5
