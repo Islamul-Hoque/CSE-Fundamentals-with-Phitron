535b73f1d9b4783f763560cadabe0f2c6040b20a
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-23 14:13:55 +0600

    feat(semester-2/module-1/Linearithmic_Complexity_O_NlogN.cpp): add linearithmic complexity example demonstrating nested O(N) and O(logN) loops resulting in O(N*logN)

--- Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-1(Time and Space Complexity)/Linearithmic_Complexity_O_NlogN.cpp
+++ Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-1(Time and Space Complexity)/Linearithmic_Complexity_O_NlogN.cpp
@@ -1,21 +1,21 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cin >> n;
+int main(){
+    int n;
+    cin >> n;
 
-//     // EX-1
-//     for (int i = 1; i <=n; i++)    // o(N)
-//         for (int j = 1; j <=n; j*=2) {  // o(logN)
-//             cout << "ISHFAK" << endl;
-//         }
+    // EX-1
+    for (int i = 1; i <=n; i++)    // o(N)
+        for (int j = 1; j <=n; j*=2) {  // o(logN)
+            cout << "ISHFAK" << endl;
+        }
 
-//     return 0;
-// }
+    return 0;
+}
 
 
-// /*
-// --------------------------------------------------------------------
-//                 Total time complexity ==> o(N*log(n)) 
-// */
+/*
+--------------------------------------------------------------------
+                Total time complexity ==> o(N*log(n)) 
+*/
