01b0b1178998f3ee3648e91af2c8f371c0851851
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-23 14:16:09 +0600

    feat(semester-2/module-1/Space_Complexity.cpp): add space complexity examples demonstrating O(1) variables, O(n) arrays, and O(n^2) 2D arrays

--- Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-1(Time and Space Complexity)/Space_Complexity.cpp
+++ Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-1(Time and Space Complexity)/Space_Complexity.cpp
@@ -1,24 +1,24 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n;        // o(1)
-//     cin >> n;
-//     int sum = 0;  // o(1)
+int main(){
+    int n;        // o(1)
+    cin >> n;
+    int sum = 0;  // o(1)
 
-//     // Ex-1
-//     for (int i = 0; i < n; i++)   // o(N)
-//         sum +=i;
+    // Ex-1
+    for (int i = 0; i < n; i++)   // o(N)
+        sum +=i;
 
-//     // EX-2
-//     int a[n];  // o(n)
-//     for (int i = 0; i < n; i++)   // o(N)
-//         cin >> a[i];
+    // EX-2
+    int a[n];  // o(n)
+    for (int i = 0; i < n; i++)   // o(N)
+        cin >> a[i];
 
-//     // EX-3
-//     int a[n][n];  // o(n*n)
-//     for (int i = 0; i < n; i++)   // o(n*n)
-//         cin >> a[i][i];
+    // EX-3
+    int a[n][n];  // o(n*n)
+    for (int i = 0; i < n; i++)   // o(n*n)
+        cin >> a[i][i];
 
-//     return 0;
-// }
+    return 0;
+}
