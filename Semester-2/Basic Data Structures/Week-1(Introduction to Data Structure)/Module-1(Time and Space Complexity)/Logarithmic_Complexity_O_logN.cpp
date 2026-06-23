9d63b85ac852d194442a9a090adc4b485ab4e127
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-23 13:57:04 +0600

    feat(semester-2/module-1/Logarithmic_Complexity_O_logN.cpp): add logarithmic complexity examples demonstrating O(logN) loops with multiply, divide, and custom increment cases

--- Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-1(Time and Space Complexity)/Logarithmic_Complexity_O_logN.cpp
+++ Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-1(Time and Space Complexity)/Logarithmic_Complexity_O_logN.cpp
@@ -1,29 +1,29 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cin >> n;
+int main(){
+    int n;
+    cin >> n;
 
-//     // Ex-1
-//     // Increment by multifly ==> i*=2
-//     for (int i = 1; i <=n; i*=2){ // o(log N)
-//         cout << i << endl;
-//     }
+    // Ex-1
+    // Increment by multifly ==> i*=2
+    for (int i = 1; i <=n; i*=2){ // o(log N)
+        cout << i << endl;
+    }
 
-//     // Ex-2
-//     // Decrement by division ==> i/=2
-//     for (int i = n; i >=1; i/=2){ // o(log N)
-//         cout << i << endl;
-//     }
+    // Ex-2
+    // Decrement by division ==> i/=2
+    for (int i = n; i >=1; i/=2){ // o(log N)
+        cout << i << endl;
+    }
 
-//     // Ex-3
-//     int k =2;
-//     for (int i = 1; i <=n; i++) { 
-//             cout << i << endl;
-//             i +=k;                // o(log N)
-//         }
+    // Ex-3
+    int k =2;
+    for (int i = 1; i <=n; i++) { 
+            cout << i << endl;
+            i +=k;                // o(log N)
+        }
 
-//     return 0;
-// }
+    return 0;
+}
 
