cb7c0307f87e57c736f53d1531e358d66bfea78a
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-23 14:12:41 +0600

    feat(semester-2/module-1/Quadratic_complexity_O_NN.cpp): add quadratic and nested loop complexity examples including O(N^2), O(N*M), and O(N*M*p) cases

--- Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-1(Time and Space Complexity)/Quadratic_complexity_O_NN.cpp
+++ Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-1(Time and Space Complexity)/Quadratic_complexity_O_NN.cpp
@@ -1,32 +1,32 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n, m, p;
-//     cin >> n >> m >> p;
+int main(){
+    int n, m, p;
+    cin >> n >> m >> p;
 
-//     // EX-1
-//     for (int i = 0; i <n; i++)    // o(N*N)
-//     for (int j = 0; j <n; j++) {
-//         cout << "ISHFAK" << endl;
-//     }
+    // EX-1
+    for (int i = 0; i <n; i++)    // o(N*N)
+    for (int j = 0; j <n; j++) {
+        cout << "ISHFAK" << endl;
+    }
 
-//     cout << "===============" << endl;
+    cout << "===============" << endl;
 
-//     // EX-2
-//     for (int i = 0; i <n; i++)    // o(N*M)
-//     for (int j = 0; j <m; j++) {
-//         cout << "Nayeem" << endl;
-//     }
+    // EX-2
+    for (int i = 0; i <n; i++)    // o(N*M)
+    for (int j = 0; j <m; j++) {
+        cout << "Nayeem" << endl;
+    }
 
-//     cout << "===============" << endl;
+    cout << "===============" << endl;
 
-//     // EX-3
-//     for (int i = 0; i <n; i++)    // o(N*M*p)
-//     for (int j = 0; j <m; j++) 
-//     for (int k = 0; k <p; k++) {
-//         cout << "Nayeem" << endl;
-//     }
+    // EX-3
+    for (int i = 0; i <n; i++)    // o(N*M*p)
+    for (int j = 0; j <m; j++) 
+    for (int k = 0; k <p; k++) {
+        cout << "Nayeem" << endl;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
