e5d865e21ebc35dca58fb8a77680416f4edce000
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-23 14:15:23 +0600

    feat(semester-2/module-1/sum.cpp): add sum of first n numbers using loop (O(n)) and formula (O(1)) to demonstrate time complexity differences

--- Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-1(Time and Space Complexity)/sum.cpp
+++ Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-1(Time and Space Complexity)/sum.cpp
@@ -1,20 +1,20 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n, s =0;
-//     cin >> n;
+int main(){
+    int n, s =0;
+    cin >> n;
 
-//     // Way-1: Loop ==> o(n)
-//     for (int i = 1; i <=n; i++)
-//         s+=i;
+    // Way-1: Loop ==> o(n)
+    for (int i = 1; i <=n; i++)
+        s+=i;
 
-//     // Way-2: Formula ==> o(1)
-//     int sum = (n*(n+1))/2;
+    // Way-2: Formula ==> o(1)
+    int sum = (n*(n+1))/2;
 
-//     cout << "Using Loop =====> " << s << endl 
-//     << "Using formula ==> " << sum << endl;
+    cout << "Using Loop =====> " << s << endl 
+    << "Using formula ==> " << sum << endl;
 
-//     return 0;
-// }
+    return 0;
+}
 
