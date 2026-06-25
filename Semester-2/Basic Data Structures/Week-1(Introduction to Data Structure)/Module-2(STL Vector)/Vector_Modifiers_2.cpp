6d0bf79d8d0af1cfe2da26382668ccccc7e8fbf9
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-25 23:32:57 +0600

    feat(semester-2/module-2/Vector_Modifiers_2.cpp): add vector modifier examples using replace for element substitution and find for search operations

--- Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-2(STL Vector)/Vector_Modifiers_2.cpp
+++ Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-2(STL Vector)/Vector_Modifiers_2.cpp
@@ -1,42 +1,42 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
+int main(){
 
-//     // 6.replace(v.begin(),v.end(),value,replace_value)
+    // 6.replace(v.begin(),v.end(),value,replace_value)
 
-//     vector<int> v ={1, 2, 3, 2, 5, 6, 2, 2};
-//     cout << "Original vector: ";
-//     for(int x : v) cout << x << " ";
+    vector<int> v ={1, 2, 3, 2, 5, 6, 2, 2};
+    cout << "Original vector: ";
+    for(int x : v) cout << x << " ";
 
-//     cout << endl  << endl << "-------- Divider --------" << endl;;
+    cout << endl  << endl << "-------- Divider --------" << endl;;
 
-//     // Replace All 
-//     replace(v.begin(), v.end(), 2, 100);
-//     cout << "Replaced all 2 with 100: " << endl;
-//     for(int x : v) cout << x << " ";
+    // Replace All 
+    replace(v.begin(), v.end(), 2, 100);
+    cout << "Replaced all 2 with 100: " << endl;
+    for(int x : v) cout << x << " ";
 
 
-//     cout << endl  << endl << "-------- Divider --------" << endl;
+    cout << endl  << endl << "-------- Divider --------" << endl;
 
-//     // Range base replace 
-//     replace(v.begin(), v.end()-1, 100, 2);
-//     cout << "replace all 2 without last 100: " << endl;
-//     for(int x : v)  cout << x << " ";
+    // Range base replace 
+    replace(v.begin(), v.end()-1, 100, 2);
+    cout << "replace all 2 without last 100: " << endl;
+    for(int x : v)  cout << x << " ";
 
-//     cout << endl  << endl << "-------- Divider --------" << endl;
+    cout << endl  << endl << "-------- Divider --------" << endl;
 
-//     // 7.find(v.begin(),v.end(),V)
-//     vector<int> v1 ={1, 2, 3, 2, 5, 6, 2, 2};
+    // 7.find(v.begin(),v.end(),V)
+    vector<int> v1 ={1, 2, 3, 2, 5, 6, 2, 2};
 
-//     // way-1
-//     vector<int> ::iterator it = find(v1.begin(), v1.end(), 800);
+    // way-1
+    vector<int> ::iterator it = find(v1.begin(), v1.end(), 800);
 
-//     // Way-2
-//     auto it2 = find(v1.begin(), v1.end(), 2);
+    // Way-2
+    auto it2 = find(v1.begin(), v1.end(), 2);
 
-//     if(it2 == v1.end())  cout << "Not found" << endl;
-//     else cout << "Found" << endl;
+    if(it2 == v1.end())  cout << "Not found" << endl;
+    else cout << "Found" << endl;
 
-//     return 0;
-// }
+    return 0;
+}
