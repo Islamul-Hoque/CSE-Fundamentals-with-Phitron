aaf95c8bbcf78b8011ca384a8545afba81ffb05b
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-25 23:39:11 +0600

    feat(semester-2/module-3/Y_Range_sum_query.cpp): add brute force solution for range sum queries with O(n*q) time complexity

--- Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-3(Prefix Sum and Binary Search)/Y_Range_sum_query.cpp
+++ Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-3(Prefix Sum and Binary Search)/Y_Range_sum_query.cpp
@@ -1,26 +1,26 @@
-// // Brute force approach (O(n*q))
-// #include <bits/stdc++.h>
-// using namespace std;
+// Brute force approach (O(n*q))
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n, q;
-//     cin >> n >> q;
-//     vector<int> v(n+1);
+int main(){
+    int n, q;
+    cin >> n >> q;
+    vector<int> v(n+1);
 
-//     for (int i = 1; i <=n; i++) 
-//         cin >> v[i];
+    for (int i = 1; i <=n; i++) 
+        cin >> v[i];
 
-//     while (q--){   // O(n*q)
-//         int l, r, sum =0;
-//         cin >> l >> r;
+    while (q--){   // O(n*q)
+        int l, r, sum =0;
+        cin >> l >> r;
 
-//         for (int i = l; i <=r; i++)
-//             sum +=v[i];
+        for (int i = l; i <=r; i++)
+            sum +=v[i];
 
-//         cout << sum << endl;
-//     }
-//     return 0;
-// }
+        cout << sum << endl;
+    }
+    return 0;
+}
 
 // // Optimized approach using prefix sum (O(q))
 // #include <bits/stdc++.h>
