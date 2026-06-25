ad87c7a64fa2e0627392f4c8fdba85e7fef4acae
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-25 23:36:21 +0600

    feat(semester-2/module-2/Vector_of_String.cpp): add vector of string input/output example using getline for multiple string entries

--- Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-2(STL Vector)/Vector_of_String.cpp
+++ Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-2(STL Vector)/Vector_of_String.cpp
@@ -1,16 +1,16 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cin >> n;
-//     cin.ignore();
-//     vector<string> v(n);
+int main(){
+    int n;
+    cin >> n;
+    cin.ignore();
+    vector<string> v(n);
     
-//     for (int i = 0; i <n; i++)
-//         getline(cin, v[i]);
+    for (int i = 0; i <n; i++)
+        getline(cin, v[i]);
 
-//     for(string s: v)
-//         cout << s <<endl;
-//     return 0;
-// }
+    for(string s: v)
+        cout << s <<endl;
+    return 0;
+}
