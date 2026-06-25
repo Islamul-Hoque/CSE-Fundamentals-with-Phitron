c7beeb14659f444b96217215217d00649bf9825e
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-25 23:35:42 +0600

    feat(semester-2/module-2/Vector_Input_And_Output.cpp): add vector input/output examples using fixed size initialization and dynamic push_back approach

--- Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-2(STL Vector)/Vector_Input_And_Output.cpp
+++ Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-2(STL Vector)/Vector_Input_And_Output.cpp
@@ -1,29 +1,29 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n, m ;
-//     cin >> n >> m;
+int main(){
+    int n, m ;
+    cin >> n >> m;
 
-//     // Way-1
-//     vector<int> v(n);
-//     cout << "Way-1: ";
-//     for(int i =0; i<n; i++) 
-//         cin >> v[i];
+    // Way-1
+    vector<int> v(n);
+    cout << "Way-1: ";
+    for(int i =0; i<n; i++) 
+        cin >> v[i];
 
-//     for(int x : v) cout << x << " ";
-//     cout << endl;
+    for(int x : v) cout << x << " ";
+    cout << endl;
 
-//     // Way-2
-//     cout << "Way-2: ";
-//     vector<int> v2;
-//     for(int i =0; i<m; i++) {
-//         int x;
-//         cin >> x;
-//         v2.push_back(x);
-//     }
+    // Way-2
+    cout << "Way-2: ";
+    vector<int> v2;
+    for(int i =0; i<m; i++) {
+        int x;
+        cin >> x;
+        v2.push_back(x);
+    }
 
-//     for(int x : v2) cout << x << " ";
+    for(int x : v2) cout << x << " ";
 
-//     return 0;
-// }
+    return 0;
+}
