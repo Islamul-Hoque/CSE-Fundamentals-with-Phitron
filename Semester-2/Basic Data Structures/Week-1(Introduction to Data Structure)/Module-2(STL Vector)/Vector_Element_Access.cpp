1e2ab3d3dc418bfbeb0d801db3f6e29661bc3333
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-25 23:33:50 +0600

    feat(semester-2/module-2/Vector_Element_Access.cpp): add vector element access examples using index operator, at(), back(), front(), and size for last index

--- Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-2(STL Vector)/Vector_Element_Access.cpp
+++ Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-2(STL Vector)/Vector_Element_Access.cpp
@@ -1,22 +1,22 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
+int main(){
 
-//     // 1.Access the ith element
-//     vector<int> v ={1, 2, 3, 4, 5, 6, 7, 8};
+    // 1.Access the ith element
+    vector<int> v ={1, 2, 3, 4, 5, 6, 7, 8};
 
-//     // 1. Access the ith element
-//     cout << "Element at index 3 (using []): " << v[3] << endl;
-//     cout << "Element at index 3 (using at()): " << v.at(3) << endl << endl;
+    // 1. Access the ith element
+    cout << "Element at index 3 (using []): " << v[3] << endl;
+    cout << "Element at index 3 (using at()): " << v.at(3) << endl << endl;
 
-//     // 2. Access the last element
-//     cout << "Last element (using back()): " << v.back() << endl;
-//     cout << "Last index (size-1): " << v.size() << endl << endl;
+    // 2. Access the last element
+    cout << "Last element (using back()): " << v.back() << endl;
+    cout << "Last index (size-1): " << v.size() << endl << endl;
 
-//     // 3. Access the first element
-//     cout << "First element (using front()): " << v.front() << endl;
-//     cout << "First element (using v[0]): " << v[0] << endl;
+    // 3. Access the first element
+    cout << "First element (using front()): " << v.front() << endl;
+    cout << "First element (using v[0]): " << v[0] << endl;
 
-//     return 0;
-// }
+    return 0;
+}
