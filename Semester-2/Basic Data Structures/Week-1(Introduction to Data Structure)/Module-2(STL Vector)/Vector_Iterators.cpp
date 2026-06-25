e5691a6f5de04b04894c61fa9e99631f93956f4d
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-25 23:34:36 +0600

    feat(semester-2/module-2/Vector_Iterators.cpp): add vector iterator example demonstrating traversal using begin() to end() with dereference operator

--- Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-2(STL Vector)/Vector_Iterators.cpp
+++ Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-2(STL Vector)/Vector_Iterators.cpp
@@ -1,10 +1,10 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     vector<int> v ={1, 2, 3, 4, 5};
-//     for(auto it = v.begin(); it < v.end(); it++)
-//         cout << *it << " ";
+int main(){
+    vector<int> v ={1, 2, 3, 4, 5};
+    for(auto it = v.begin(); it < v.end(); it++)
+        cout << *it << " ";
 
-//     return 0;
-// }
+    return 0;
+}
