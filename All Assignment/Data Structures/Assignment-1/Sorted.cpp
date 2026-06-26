d310df2de39f4aee073906f3780a2db96193b0ff
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-26 23:37:56 +0600

    feat(data-structures/assignment-1/Sorted.cpp): implement sorted array check for multiple test cases using non-decreasing order validation

--- All Assignment/Data Structures/Assignment-1/Sorted.cpp
+++ All Assignment/Data Structures/Assignment-1/Sorted.cpp
@@ -1,28 +1,28 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int t;
-//     cin >> t;
+int main(){
+    int t;
+    cin >> t;
 
-//     while (t--){
-//         int n, flag= 1;
-//         cin >> n;
-//         int a[n];
+    while (t--){
+        int n, flag= 1;
+        cin >> n;
+        int a[n];
 
-//         for (int i = 0; i <n; i++)
-//             cin >> a[i];
+        for (int i = 0; i <n; i++)
+            cin >> a[i];
 
-//         for (int i = 1; i < n; i++){
-//             if(a[i] < a[i-1]){
-//                 flag = 0;
-//                 break;
-//             }
-//         }
+        for (int i = 1; i < n; i++){
+            if(a[i] < a[i-1]){
+                flag = 0;
+                break;
+            }
+        }
 
-//         if(flag == 1) cout << "YES" << endl;
-//         else cout << "NO" << endl;
-//     }
+        if(flag == 1) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
