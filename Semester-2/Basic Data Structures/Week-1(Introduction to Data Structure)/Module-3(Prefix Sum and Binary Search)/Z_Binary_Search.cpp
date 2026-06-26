6108bb0a06b2ecf2b0feacc40299dbd1bdb162fe
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-26 17:46:51 +0600

    feat(semester-2/module-3/quiz.cpp): add quiz examples covering prefix sum purpose, time complexity, range sum O(1), binary search worst-case O(log n), sorted array requirement, termination condition, and cumulative sum insights

--- Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-3(Prefix Sum and Binary Search)/Z_Binary_Search.cpp
+++ Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-3(Prefix Sum and Binary Search)/Z_Binary_Search.cpp
@@ -1,30 +1,30 @@
-// // Brute force approach O(n*q)
-// #include <bits/stdc++.h>
-// using namespace std;
+// Brute force approach O(n*q)
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n, q;
-//     cin >> n >> q;
+int main(){
+    int n, q;
+    cin >> n >> q;
 
-//     int a[n];
-//     for (int  i = 0; i <n; i++)
-//         cin >> a[i];
+    int a[n];
+    for (int  i = 0; i <n; i++)
+        cin >> a[i];
 
-//     while (q--) {   // O(q)
-//         int x, flag=0;
-//         cin >> x;
+    while (q--) {   // O(q)
+        int x, flag=0;
+        cin >> x;
 
-//         for (int i = 0; i < n; i++) {  // O(n)
-//             if(a[i] == x) {
-//                 flag =1;
-//             }
-//         }
-//         if(flag == 1) cout << "found" << endl;
-//         else cout << "not found" << endl;
-//     }
+        for (int i = 0; i < n; i++) {  // O(n)
+            if(a[i] == x) {
+                flag =1;
+            }
+        }
+        if(flag == 1) cout << "found" << endl;
+        else cout << "not found" << endl;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
