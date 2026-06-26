3ddc6fa7df2504631c178b90fc2db8cc2a71fa2b
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-26 17:45:51 +0600

    feat(semester-2/module-3/Z_Binary_Search.cpp): add optimized binary search solution for element queries with O(q*log n) time complexity after sorting

--- Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-3(Prefix Sum and Binary Search)/Z_Binary_Search.cpp
+++ Semester-2/Basic Data Structures/Week-1(Introduction to Data Structure)/Module-3(Prefix Sum and Binary Search)/Z_Binary_Search.cpp
@@ -28,41 +28,41 @@
 
 
 
-// // Optimized approach using Binary Search (O(q*log(n)))
-// #include <bits/stdc++.h>
-// using namespace std;
+// Optimized approach using Binary Search (O(q*log(n)))
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n, q;
-//     cin >> n >> q;
+int main(){
+    int n, q;
+    cin >> n >> q;
 
-//     int a[n];
-//     for (int  i = 0; i <n; i++)
-//         cin >> a[i];
+    int a[n];
+    for (int  i = 0; i <n; i++)
+        cin >> a[i];
 
-//     sort(a, a+n);  // O(n*log(n))
-//     while (q--) {   // O(q)
-//         int l=0, r =n-1, val, flag=0;
-//         cin >> val;
+    sort(a, a+n);  // O(n*log(n))
+    while (q--) {   // O(q)
+        int l=0, r =n-1, val, flag=0;
+        cin >> val;
 
-//         while (l<=r){  // O(log(n))
-//             int mid = (l+r)/2;
-//             if(a[mid] == val) {
-//                 flag =1;
-//                 break;
-//             }
-//             else if(a[mid] > val) r = mid-1;
-//             else l = mid+1;
-//         }
-//         if(flag == 1) cout << "found" << endl;
-//         else cout << "not found" << endl;
-//     }
+        while (l<=r){  // O(log(n))
+            int mid = (l+r)/2;
+            if(a[mid] == val) {
+                flag =1;
+                break;
+            }
+            else if(a[mid] > val) r = mid-1;
+            else l = mid+1;
+        }
+        if(flag == 1) cout << "found" << endl;
+        else cout << "not found" << endl;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
-// /*
-// --------------------------------------------------------------------
-//                 Total time complexity   = O(n*log(n)) +(O(q) * O(log(n))) 
-//                                         ==> (O(q*log(n)))
-// */
+/*
+--------------------------------------------------------------------
+                Total time complexity   = O(n*log(n)) +(O(q) * O(log(n))) 
+                                        ==> (O(q*log(n)))
+*/
