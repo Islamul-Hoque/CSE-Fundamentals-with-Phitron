813b38c94dd298cd2c25cc57dd064246621d11fb
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-26 23:35:40 +0600

    feat(data-structures/assignment-1/Insert_it.cpp): implement vector insertion by adding one vector into another at a given index

--- All Assignment/Data Structures/Assignment-1/Insert_it.cpp
+++ All Assignment/Data Structures/Assignment-1/Insert_it.cpp
@@ -1,27 +1,27 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n, m;
-//     cin >> n;
-//     vector<int> a(n);
+int main(){
+    int n, m;
+    cin >> n;
+    vector<int> a(n);
 
-//     for (int i = 0; i <n; i++)
-//         cin >> a[i];
+    for (int i = 0; i <n; i++)
+        cin >> a[i];
 
-//     cin >> m;
-//     vector<int > b(m);
+    cin >> m;
+    vector<int > b(m);
 
-//     for (int i = 0; i <m; i++) 
-//         cin >> b[i];
+    for (int i = 0; i <m; i++) 
+        cin >> b[i];
 
-//     int x;
-//     cin >> x;
+    int x;
+    cin >> x;
 
-//     a.insert(a.begin()+x, b.begin(), b.end());
+    a.insert(a.begin()+x, b.begin(), b.end());
 
-//     for(int x : a) 
-//         cout << x << " ";
+    for(int x : a) 
+        cout << x << " ";
 
-//     return 0;
-// }
+    return 0;
+}
