1e72d8ac480fed62dbcdc464e12428cd85b276c7
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-01 02:10:28 +0600

    feat(semester-2/week-2/module-5/Node_with_Constructor.cpp): implement singly linked list node creation using constructor with linking and traversal demonstration

---
+++ Semester-2/Basic Data Structures/Week-2(Linked List)/Module-5(Singly Linked List)/Node_with_Constructor.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int val;
+    Node* next;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+int main(){
+    Node a(100), b(200), c(300);
+
+    a.next = &b;
+    b.next = &c;
+
+    cout << a.val << " " << a.next->val << " " << a.next->next->val << endl;
+
+    return 0;
+}
