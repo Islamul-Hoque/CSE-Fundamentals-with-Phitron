917a00eb2631ecaf5e5d5a3faada2bb04190626c
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-01 03:09:08 +0600

    feat(module-5/Dynamic_Node.cpp): implement singly linked list using dynamic node creation with linking and traversal demonstration

--- Semester-2/Basic Data Structures/Week-2(Linked List)/Module-5(Singly Linked List)/Dynamic_Node.cpp
+++ Semester-2/Basic Data Structures/Week-2(Linked List)/Module-5(Singly Linked List)/Dynamic_Node.cpp
@@ -1,24 +1,30 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Node{
-// public:
-//     int val;
-//     Node* next;
+class Node{
+    public:
+        int val;
+        Node* next;
 
-//     Node(int val){
-//         this->val = val;
-//         this->next = NULL;
-//     }
-// };
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
 
-// int main(){
-//     Node a(100), b(200), c(300);
+int main(){
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(300);
 
-//     a.next = &b;
-//     b.next = &c;
+    // Way-1
+    (*head).next = a;
 
-//     cout << a.val << " " << a.next->val << " " << a.next->next->val << endl;
+    // Way-2
+    head->next = a;
+    a->next =b;
 
-//     return 0;
-// }
+    cout << head->val << " " << head->next->val << " " << head->next->next->val << endl;
+
+    return 0;
+}
