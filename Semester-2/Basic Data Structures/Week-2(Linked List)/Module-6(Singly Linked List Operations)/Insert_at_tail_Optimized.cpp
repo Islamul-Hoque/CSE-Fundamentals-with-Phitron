b823f7a046edaf83a86ddfc417d8e4ed85323c5f
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-02 23:28:03 +0600

    feat(module-6/Insert_at_tail_Optimized.cpp): implement optimized tail insertion using tail pointer reference with traversal printing

---
+++ Semester-2/Basic Data Structures/Week-2(Linked List)/Module-6(Singly Linked List Operations)/Insert_at_tail_Optimized.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* next;
+
+    // constructor 
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+// insert a new node 
+void Insert_at_Tail(Node* &head, Node* &tail, int val){
+    Node* newNode = new Node(val);
+
+    // handle empty list case
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode;   // tail->next or newNode
+}
+
+// print all nodes
+void Print_Linked_List(Node* head){
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+int main(){
+    // create initial linked list with 3 nodes
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    head->next = a;
+    a->next = tail;
+
+    // insert new nodes at the tail
+    Insert_at_Tail(head, tail, 400);
+    Insert_at_Tail(head, tail, 500);
+    Insert_at_Tail(head, tail, 600);
+
+    // print the linked list
+    Print_Linked_List(head);
+    cout << "Tail: " << tail->val << endl;
+
+    return 0;
+}
---
+++ Semester-2/Basic Data Structures/Week-2(Linked List)/Module-6(Singly Linked List Operations)/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+500);
+    // Insert_at_Tail(head, tail, 600);
