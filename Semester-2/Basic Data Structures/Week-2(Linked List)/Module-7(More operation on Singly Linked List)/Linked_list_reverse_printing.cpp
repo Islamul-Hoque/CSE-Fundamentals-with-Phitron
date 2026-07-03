862d825adf283c5465887ebd90b43f6fc4f54c5a
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-03 22:59:03 +0600

    feat(module-7/Linked_list_reverse_printing.cpp): implement recursive reverse printing of singly linked list with tail-based insertion

--- Semester-2/Basic Data Structures/Week-2(Linked List)/Module-7(More operation on Singly Linked List)/Linked_list_reverse_printing.cpp
+++ Semester-2/Basic Data Structures/Week-2(Linked List)/Module-7(More operation on Singly Linked List)/Linked_list_reverse_printing.cpp
@@ -1,51 +1,51 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Node{
-//     public: 
-//         int val;
-//         Node* next;
+class Node{
+    public: 
+        int val;
+        Node* next;
 
-//         Node(int val){
-//             this->val = val;
-//             this->next = NULL;
-//         }
-// };
+        Node(int val){
+            this->val = val;
+            this->next = NULL;
+        }
+};
 
-// void Insert_At_Tail(Node* &head, Node* &tail, int val){
-//     Node* newHead = new Node(val);
+void Insert_At_Tail(Node* &head, Node* &tail, int val){
+    Node* newHead = new Node(val);
 
-//     if(head == NULL){
-//         head = newHead;
-//         tail = newHead;
-//     }
+    if(head == NULL){
+        head = newHead;
+        tail = newHead;
+    }
 
-//     tail->next = newHead;
-//     tail = newHead;
-// }
+    tail->next = newHead;
+    tail = newHead;
+}
 
-// void Print_reverse(Node* head){
-//     Node* temp = head;
+void Print_reverse(Node* head){
+    Node* temp = head;
 
-//     if(temp == NULL) return;
+    if(temp == NULL) return;
 
-//     Print_reverse(temp->next);
-//     cout << temp->val << endl;
-// }
+    Print_reverse(temp->next);
+    cout << temp->val << endl;
+}
 
-// int main(){
-//     Node* head = NULL;
-//     Node* tail = NULL;
+int main(){
+    Node* head = NULL;
+    Node* tail = NULL;
 
-//     while (true) {
-//         int val;
-//         cin >> val;
+    while (true) {
+        int val;
+        cin >> val;
 
-//         if(val == -1) break;
-//         Insert_At_Tail(head, tail, val);
-//     }
+        if(val == -1) break;
+        Insert_At_Tail(head, tail, val);
+    }
 
-//     Print_reverse(head);
+    Print_reverse(head);
 
-//     return 0;
-// }
+    return 0;
+}
