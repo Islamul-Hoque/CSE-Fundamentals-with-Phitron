bc30bd0825ede5485ea3fb0d75719a8f37279cad
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-03 23:40:02 +0600

    feat(module-7/Delete_at_head.cpp): add concise comments for constructor, tail insertion, head deletion steps, printing, and main function flow

--- Semester-2/Basic Data Structures/Week-2(Linked List)/Module-7(More operation on Singly Linked List)/Delete_at_head.cpp
+++ Semester-2/Basic Data Structures/Week-2(Linked List)/Module-7(More operation on Singly Linked List)/Delete_at_head.cpp
@@ -6,15 +6,18 @@ class Node{
         int val;
         Node* next;
 
+    // constructor
     Node(int val){
         this->val = val;
         this->next = NULL;
     }
 };
 
+// insert node at tail
 void Insert_at_Tail(Node* &head, Node* &tail, int val){
     Node* newNode = new Node(val);
 
+    // empty list case
     if(head == NULL){
         head = newNode;
         tail = newNode;
@@ -25,15 +28,23 @@ void Insert_at_Tail(Node* &head, Node* &tail, int val){
     tail = newNode;
 }
 
+// delete node at head
 void Delete_At_Head(Node* &head){
+    // store current head node to delete later
     Node* deleteNode = head;
+
+    // move head pointer to next node
     head = head->next;
 
+    // free memory of old head
     delete deleteNode;
 }
 
+
+// print linked list
 void Print_Linked_List(Node* head){
     Node* temp = head;
+
     while (temp != NULL) {
         cout << temp->val << endl;
         temp = temp->next;
@@ -44,16 +55,16 @@ int main(){
     Node* head = NULL;
     Node* tail = NULL;
 
+    // input until -1
     int val;
     while(true){
         cin >> val;
-
         if(val == -1) break;
         Insert_at_Tail(head, tail, val);
     }
 
-    Delete_At_Head(head);
-    Print_Linked_List(head);
+    Delete_At_Head(head);   // delete first node
+    Print_Linked_List(head); // print remaining list
 
     return 0;
 }
