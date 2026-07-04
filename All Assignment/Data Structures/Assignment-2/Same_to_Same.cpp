702e121591726d8918f7571ce0adaa435afb575b
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-05 00:13:30 +0600

    feat(Assignment-2/Same_to_Same.cpp): deliver complete linked list comparison workflow with dual list input, tail insertion, node-by-node equality check, and result output

--- All Assignment/Data Structures/Assignment-2/Same_to_Same.cpp
+++ All Assignment/Data Structures/Assignment-2/Same_to_Same.cpp
@@ -1,75 +1,75 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Node{
-//     public:
-//         int val;
-//         Node* next;
+class Node{
+    public:
+        int val;
+        Node* next;
 
-//     Node(int val){
-//         this->val = val;
-//         this->next = NULL;
-//     }
-// };
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
 
-// void Insert_at_Tail(Node* &head, Node* &tail, int val){
-//     Node* newNode = new Node(val);
+void Insert_at_Tail(Node* &head, Node* &tail, int val){
+    Node* newNode = new Node(val);
 
-//     if(head == NULL){
-//         head = newNode;
-//         tail = newNode;
-//         return;
-//     }
+    if(head == NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
 
-//     tail->next = newNode;
-//     tail = newNode;
-// }
+    tail->next = newNode;
+    tail = newNode;
+}
 
-// void Compare_Linked_List(Node* head, Node* head2){
-//     Node* temp = head;
-//     Node* temp2 = head2;
+void Compare_Linked_List(Node* head, Node* head2){
+    Node* temp = head;
+    Node* temp2 = head2;
 
-//     int flag = 1;
+    int flag = 1;
 
-//     while (temp != NULL && temp2 != NULL) {
-//         if(temp->val != temp2->val ) {
-//             flag = 0; 
-//             break;
-//         }
-//         temp = temp->next;
-//         temp2 = temp2->next;
-//     }
+    while (temp != NULL && temp2 != NULL) {
+        if(temp->val != temp2->val ) {
+            flag = 0; 
+            break;
+        }
+        temp = temp->next;
+        temp2 = temp2->next;
+    }
 
-//     if(temp != NULL || temp2 != NULL) flag = 0;
+    if(temp != NULL || temp2 != NULL) flag = 0;
 
-//     if(flag == 1) cout << "YES" << endl;
-//     else cout << "NO" << endl;
-// }
+    if(flag == 1) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
 
-// int main(){
-//     Node* head = NULL;
-//     Node* tail = NULL;
-//     int val;
+int main(){
+    Node* head = NULL;
+    Node* tail = NULL;
+    int val;
 
-//     while(true){
-//         cin >> val;
+    while(true){
+        cin >> val;
 
-//         if(val == -1) break;
-//         Insert_at_Tail(head, tail, val);
-//     }
+        if(val == -1) break;
+        Insert_at_Tail(head, tail, val);
+    }
 
-//     Node* head2 = NULL;
-//     Node* tail2 = NULL;
-//     int val2;
+    Node* head2 = NULL;
+    Node* tail2 = NULL;
+    int val2;
 
-//     while(true){
-//         cin >> val2;
+    while(true){
+        cin >> val2;
 
-//         if(val2 == -1) break;
-//         Insert_at_Tail(head2, tail2, val2);
-//     }
+        if(val2 == -1) break;
+        Insert_at_Tail(head2, tail2, val2);
+    }
 
-//     Compare_Linked_List(head, head2);
+    Compare_Linked_List(head, head2);
 
-//     return 0;
-// }
+    return 0;
+}
