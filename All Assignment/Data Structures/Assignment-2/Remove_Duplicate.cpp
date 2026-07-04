97333dbcea5725234cbfd6bf0290dcbd546df4b9
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-05 00:18:36 +0600

    docs(Assignment-2/Remove_Duplicate.cpp): add detailed problem analysis, observation, and testcase explanation for linked list duplicate removal

--- All Assignment/Data Structures/Assignment-2/Remove_Duplicate.cpp
+++ All Assignment/Data Structures/Assignment-2/Remove_Duplicate.cpp
@@ -72,3 +72,31 @@ int main(){
 
     return 0;
 }
+
+
+/*
+
+Question Analysis : 
+আমাকে একটা singly linked list ইনপুট নিতে হবে। এই লিস্ট থেকে duplicate value গুলাকে ডিলিট করে List টা Print করতে হবে। List এ কোন duplicate value পাওয়া না গেলে ইনপুট নেওয়া List টা Print করে দিব।
+
+Observation : 
+N এবং Vএর constants অনুযায়ী  variable এর data type নিতে হবে। 
+singly linked list এর value গুলা ইনপুট নিতে নিতে লাস্টে -1 পেলে ইনপুট থামিয়ে দিব। 
+
+Testcase Analysis :
+1 2 4 2 3 5 1 4 5 2 6 1 -1
+
+এই Testcase এ,
+1, 2, 4, 5 এই 4টা value ডুপ্লিকেট আছে।
+এই ডুপ্লিকেট value ডিলিট করার জন্য 2টা pointer অর্থাৎ Outer pointer এবং Inner pointer নিতে হবে।
+
+Outer pointer (current), প্রতিটি node কে ধরবে। এবং Inner pointer (temp) এর কাজ হলো current node এর পরের অংশে গিয়ে duplicate খুঁজে বের করা।
+
+যখনই duplicate পাওয়া যাবে, inner pointer সেই node কে delete করে দিবে।
+
+এভাবে প্রতিটি node এর duplicate ডিলিট  করার পর লিস্টে শুধু unique মানগুলো থেকে যাবে।
+
+এই singly linked list এর duplicate value গুলোকে ডিলিট পরার পর এটিকে print করলে output আসবে: 
+1 2 4 3 5 6
+
+*/
