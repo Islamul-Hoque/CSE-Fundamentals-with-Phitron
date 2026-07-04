3d1c40acdd134b395b8b113c09df9cb2ab0a30af
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-05 00:20:52 +0600

    docs(Assignment-1/Get_Prefix_Sum.cpp): add detailed problem analysis, observation, and testcase explanation for prefix sum reverse printing

--- All Assignment/Data Structures/Assignment-1/Get_Prefix_Sum.cpp
+++ All Assignment/Data Structures/Assignment-1/Get_Prefix_Sum.cpp
@@ -22,3 +22,26 @@ int main(){
 
     return 0;
 }
+
+
+/*
+
+Question Analysis :
+আমাকে একটা N সাইজের A নামক array দিবে। এই array কে prefix sum করে নতুন array বানাতে হবে। ওই নতুন array কে reverse order এ print করতে হবে।
+Observation :
+N এবং A[i] এর constants অনুযায়ী variable এর data type নিতে হবে।
+Testcase Analysis :
+5
+2 4 1 5 3
+এই Testcase এর প্রথম লাইনের 5 হলো N এর মান। দ্বিতীয় লাইনে 5 সাইজের array এর এলিমেন্ট গুলা দিয়েছে।
+এখানে, [2 4 1 5 3] এই 5 সাইজের array এর prefix sum নির্ণয়:
+pre[0] = 2
+pre[1] = 2 + 4 = 6
+pre[2] = 6 + 1 = 7
+pre[3] = 7 + 5 = 12
+pre[4] = 12 + 3 = 15
+Prefix Sum Array: [2 6 7 12 15]
+এই prefix sum array কে reverse order এ print করলে output আসবে:
+15 12 7 6 2
+
+*/
