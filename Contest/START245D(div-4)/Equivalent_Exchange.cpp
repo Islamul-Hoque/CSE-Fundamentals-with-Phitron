37fbc0d4b6f3cf86417d90e648a035b8b8dfe8c1
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-05 00:35:07 +0600

    feat(Contest/START245D/Cooling_Conundrum.cpp): add competitive programming solution with multi-test input, loop-based summation, and ceil division handling

--- Contest/START245D(div-4)/Equivalent_Exchange.cpp
+++ Contest/START245D(div-4)/Equivalent_Exchange.cpp
@@ -1,45 +1,45 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-int main(){
-    int t;
-    cin >> t;
+// int main(){
+//     int t;
+//     cin >> t;
 
-    while (t--) {
-        int n, k, possible = 0;
-        cin >> n >> k;
+//     while (t--) {
+//         int n, k, possible = 0;
+//         cin >> n >> k;
 
-        vector<int> v(n);
-        for(int i=0; i<n; i++)
-            cin >> v[i];
+//         vector<int> v(n);
+//         for(int i=0; i<n; i++)
+//             cin >> v[i];
 
-        for (int x = 0; x<=k; x++){
-            int red = x, blue = k-x, flag = 1;
+//         for (int x = 0; x<=k; x++){
+//             int red = x, blue = k-x, flag = 1;
 
-            for(int a: v){
-                if(a> 0) {
-                    red +=a;
-                    blue -=a;
-                }
-                else if(a< 0) {
-                    red -= (-a);
-                    blue +=(-a);
-                }
-                if(red < 0 || blue < 0){
-                    flag = 0;
-                    break;
-                }
+//             for(int a: v){
+//                 if(a> 0) {
+//                     red +=a;
+//                     blue -=a;
+//                 }
+//                 else if(a< 0) {
+//                     red -= (-a);
+//                     blue +=(-a);
+//                 }
+//                 if(red < 0 || blue < 0){
+//                     flag = 0;
+//                     break;
+//                 }
 
-            }
-            if(flag == 1){
-                possible = 1;
-                break;
-            }
-        }
+//             }
+//             if(flag == 1){
+//                 possible = 1;
+//                 break;
+//             }
+//         }
 
-        if(possible == 1) cout << "Yes" << endl;
-        else cout << "No" << endl;
-    }
+//         if(possible == 1) cout << "Yes" << endl;
+//         else cout << "No" << endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
