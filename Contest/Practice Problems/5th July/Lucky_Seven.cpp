c220daee5e1b2987cfd35e1576be48f665543eac
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-06 23:14:31 +0600

    feat(Contest/Practice_Problems/Lucky_Seven.cpp): add string indexing solution to print the seventh character from input

--- Contest/Practice Problems/5th July/Lucky_Seven.cpp
+++ Contest/Practice Problems/5th July/Lucky_Seven.cpp
@@ -1,11 +1,11 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     string s;
-//     cin >> s;
+int main(){
+    string s;
+    cin >> s;
 
-//     cout << s[6] << endl;
+    cout << s[6] << endl;
 
-//     return 0;
-// }
+    return 0;
+}
