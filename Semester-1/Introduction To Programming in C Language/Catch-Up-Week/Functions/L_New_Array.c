a24e6c12bdf1249cc64f73caa3420fa893fbb08a
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-07 03:59:21 +0600

    feat(Catch-Up-Week/Functions/L_New_Array.c): implement array concatenation function to merge B followed by A into new array and print sequentially

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Functions/L_New_Array.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Functions/L_New_Array.c
@@ -1,30 +1,30 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// void New_Array(int a[], int b[], int n){
-//     int c[2*n];
+void New_Array(int a[], int b[], int n){
+    int c[2*n];
 
-//     for (int i = 0; i <n; i++)
-//         c[i] = b[i];
+    for (int i = 0; i <n; i++)
+        c[i] = b[i];
 
-//     for (int i = 0; i <n; i++)
-//         c[i+n] = a[i];
+    for (int i = 0; i <n; i++)
+        c[i+n] = a[i];
 
-//     for (int i = 0; i <2*n; i++)
-//         printf("%d ", c[i]);
-// }
+    for (int i = 0; i <2*n; i++)
+        printf("%d ", c[i]);
+}
 
-// int main() {
-//     int n;
-//     scanf("%d", &n);
-//     int a[n], b[n];
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a[n], b[n];
 
-//     for (int i = 0; i <n; i++)
-//         scanf("%d", &a[i]);
+    for (int i = 0; i <n; i++)
+        scanf("%d", &a[i]);
 
-//     for (int i = 0; i <n; i++)
-//         scanf("%d", &b[i]);
+    for (int i = 0; i <n; i++)
+        scanf("%d", &b[i]);
 
-//     New_Array(a, b, n);
+    New_Array(a, b, n);
 
-//     return 0;
-// }
+    return 0;
+}
