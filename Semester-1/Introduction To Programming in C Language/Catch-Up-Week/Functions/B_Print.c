e4b11c2975a3dfeb524ff982b97daea15cfcbdc3
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-07 03:54:18 +0600

    feat(Semester-1/Catch-Up-Week/Functions/B_Print.c): add function-based solution to print numbers from 1 to n with space separation

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Functions/B_Print.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Functions/B_Print.c
@@ -1,16 +1,16 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// void print(int n){
-//     for (int i = 1; i <=n; i++){
-//         printf("%d", i);
-//         if(i<n) printf(" ");
-//     }
-// }
+void print(int n){
+    for (int i = 1; i <=n; i++){
+        printf("%d", i);
+        if(i<n) printf(" ");
+    }
+}
 
-// int main() {
-//     int n;
-//     scanf("%d", &n);
-//     print(n);
+int main() {
+    int n;
+    scanf("%d", &n);
+    print(n);
 
-//     return 0;
-// }
+    return 0;
+}
