9cbfe34308e6d14bceb2450abcd4841a4709a97f
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-07 03:55:15 +0600

    feat(Semester-1/Catch-Up-Week/Functions/D_Prime_Function.c): add optimized prime checking solution using sqrt(n) loop with multi-test case support

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Functions/D_Prime_Function.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Functions/D_Prime_Function.c
@@ -1,33 +1,33 @@
-// #include <stdio.h>
-// #include <math.h>
+#include <stdio.h>
+#include <math.h>
 
-// void Prime(int n){
-//     int count = 1;
+void Prime(int n){
+    int count = 1;
 
-//     if(n < 2){
-//         printf("NO\n");
-//         return;
-//     }
+    if(n < 2){
+        printf("NO\n");
+        return;
+    }
 
-//     for (int i = 2; i <=sqrt(n); i++){
-//         if(n%i == 0){
-//             count = 0;
-//             break;
-//         }
-//     }
-//     if(count == 1) printf("YES\n");
-//     else printf("NO\n");
-// }
+    for (int i = 2; i <=sqrt(n); i++){
+        if(n%i == 0){
+            count = 0;
+            break;
+        }
+    }
+    if(count == 1) printf("YES\n");
+    else printf("NO\n");
+}
 
-// int main() {
-//     int t;
-//     scanf("%d", &t);
+int main() {
+    int t;
+    scanf("%d", &t);
 
-//     while (t--){
-//         int n;
-//         scanf("%d", &n);
-//         Prime(n);
-//     }
+    while (t--){
+        int n;
+        scanf("%d", &n);
+        Prime(n);
+    }
 
-//     return 0;
-// }
+    return 0;
+}
