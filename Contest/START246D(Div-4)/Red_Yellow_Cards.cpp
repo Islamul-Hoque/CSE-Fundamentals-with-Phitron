419a5371c0a97ff4e135e016d2634683d5d6f41e
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-08 01:30:59 +0600

    feat(Contest/START245D(Div-4)/Plant.cpp): add tree planting solution to maximize height using consecutive day minimums

--- Contest/START246D(Div-4)/Red_Yellow_Cards.cpp
+++ Contest/START246D(Div-4)/Red_Yellow_Cards.cpp
@@ -1,18 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-int main() {
-    int T;
-    cin >> T;
+// int main() {
+//     int T;
+//     cin >> T;
 
-    while (T--) {
-        long long R, Y;
-        cin >> R >> Y;
+//     while (T--) {
+//         long long R, Y;
+//         cin >> R >> Y;
 
-        long long absorbed = min(R, Y);   // 2 , 6 = 2
-        long long remaining = Y - absorbed;  // 6-2 = 4
-        long long ans = R + remaining / 2;  // 2 + 2/2 = 4
-        cout << ans << "\n";
-    }
-    return 0;
-}
+//         long long absorbed = min(R, Y);   // 2 , 6 = 2
+//         long long remaining = Y - absorbed;  // 6-2 = 4
+//         long long ans = R + remaining / 2;  // 2 + 2/2 = 4
+//         cout << ans << "\n";
+//     }
+//     return 0;
+// }
