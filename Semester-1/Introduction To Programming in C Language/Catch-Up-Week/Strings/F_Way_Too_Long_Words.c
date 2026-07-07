64079ca730d64fbf928ac544558656c0ba38e58f
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-08 01:16:34 +0600

    feat(Catch-Up-Week/Strings/F_Way_Too_Long_Words.c): add contest-style abbreviation solution for long words with multi-test case support

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Strings/F_Way_Too_Long_Words.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Strings/F_Way_Too_Long_Words.c
@@ -1,21 +1,21 @@
-// #include <stdio.h>
-// #include <string.h>
+#include <stdio.h>
+#include <string.h>
 
-// int main() {
-//     int t;
-//     scanf("%d", &t);
+int main() {
+    int t;
+    scanf("%d", &t);
 
-//     while (t--){
-//         char s[1000];
-//         scanf("%s", &s);
+    while (t--){
+        char s[1000];
+        scanf("%s", &s);
 
-//         int len = strlen(s);
+        int len = strlen(s);
 
-//         if(len > 10)
-//             printf("%c%d%c\n", s[0], len-2, s[len-1]);
-//         else 
-//             printf("%s\n",s);
-//     }
+        if(len > 10)
+            printf("%c%d%c\n", s[0], len-2, s[len-1]);
+        else 
+            printf("%s\n",s);
+    }
 
-//     return 0;
-// }
+    return 0;
+}
