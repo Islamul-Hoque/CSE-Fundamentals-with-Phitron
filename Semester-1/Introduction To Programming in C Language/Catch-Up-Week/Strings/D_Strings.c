372a93926008dcdb3fbb84527cf9ae3a0370038c
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-08 01:14:47 +0600

    feat(Catch-Up-Week/Strings/D_Strings.c): add string manipulation solution to print lengths, concatenate, and swap first characters

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Strings/D_Strings.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Strings/D_Strings.c
@@ -1,20 +1,20 @@
-// #include <stdio.h>
-// #include <string.h>
+#include <stdio.h>
+#include <string.h>
 
-// int main() {
-//     char s1[11], s2[11];
+int main() {
+    char s1[11], s2[11];
 
-//     scanf("%s %s", &s1, &s2);
+    scanf("%s %s", &s1, &s2);
 
-//     printf("%d %d\n", strlen(s1), strlen(s2));
-//     printf("%s%s\n", s1,s2);
+    printf("%d %d\n", strlen(s1), strlen(s2));
+    printf("%s%s\n", s1,s2);
 
 
-//     char temp = s1[0];
-//     s1[0] = s2[0];
-//     s2[0] = temp;
+    char temp = s1[0];
+    s1[0] = s2[0];
+    s2[0] = temp;
 
-//     printf("%s %s\n", s1,s2);
+    printf("%s %s\n", s1,s2);
 
-//     return 0;
-// }
+    return 0;
+}
