72c1519632a316b6b422b5874cc73c166141e1a8
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-08 01:18:23 +0600

    feat(Catch-Up-Week/Strings/I_Palindrome.c): add palindrome checking solution using two-pointer approach with YES/NO output

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Strings/I_Palindrome.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Strings/I_Palindrome.c
@@ -1,19 +1,19 @@
-// #include <stdio.h>
-// #include <string.h>
+#include <stdio.h>
+#include <string.h>
 
-// int main() {
-//     char s[1001];
-//     scanf("%s", &s);
+int main() {
+    char s[1001];
+    scanf("%s", &s);
 
-//     int len = strlen(s), flag =1;
-//     for (int i = 0, j = len-1;  i <j; i++, j--) {
-//         if(s[i] != s[j]){
-//             flag = 0;
-//             break;
-//         }
-//     }
-//     if(flag == 1) printf("YES\n");
-//     else  printf("NO\n");
+    int len = strlen(s), flag =1;
+    for (int i = 0, j = len-1;  i <j; i++, j--) {
+        if(s[i] != s[j]){
+            flag = 0;
+            break;
+        }
+    }
+    if(flag == 1) printf("YES\n");
+    else  printf("NO\n");
 
-//     return 0;
-// }
+    return 0;
+}
