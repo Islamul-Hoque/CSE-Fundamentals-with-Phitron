68871f5029bb36656b5da5a40e5fd619cdcbad10
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-08 01:20:13 +0600

    feat(Catch-Up-Week/Strings/M_Subsequence_String.c): add subsequence checking solution to detect 'hello' using two-pointer traversal

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Strings/M_Subsequence_String.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Strings/M_Subsequence_String.c
@@ -1,22 +1,22 @@
-// #include <stdio.h>
-// #include <string.h>
+#include <stdio.h>
+#include <string.h>
 
-// int main() {
-//     char s[10001];
-//     scanf("%s", &s);
+int main() {
+    char s[10001];
+    scanf("%s", &s);
 
-//     char hello[] = "hello";
-//     int len = strlen(s);
+    char hello[] = "hello";
+    int len = strlen(s);
 
-//     int j =0;
-//     for (int i = 0; i <len && j <5; i++) {
-//         if(s[i] == hello[j]){
-//             j++;
-//         }
-//     }
+    int j =0;
+    for (int i = 0; i <len && j <5; i++) {
+        if(s[i] == hello[j]){
+            j++;
+        }
+    }
 
-//     if(j == 5) printf("YES");
-//     else printf("NO");
+    if(j == 5) printf("YES");
+    else printf("NO");
 
-//     return 0;
-// }
+    return 0;
+}
