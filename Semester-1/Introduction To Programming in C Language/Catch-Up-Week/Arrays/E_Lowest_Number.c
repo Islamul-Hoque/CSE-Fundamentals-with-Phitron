de8dd04b6753db98429ed4277bf38a3212b6332c
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-09 22:41:52 +0600

    feat(Catch-Up-Week/Arrays/E_Lowest_Number.c): add solution to find lowest number and its 1-indexed position in array

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Arrays/E_Lowest_Number.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Arrays/E_Lowest_Number.c
@@ -1,20 +1,20 @@
-// #include <stdio.h>
-// #include <limits.h>
+#include <stdio.h>
+#include <limits.h>
 
-// int main(){
-//     int n;
-//     scanf("%d", &n);
+int main(){
+    int n;
+    scanf("%d", &n);
 
-//     int a[n], mn = INT_MAX, idx;
-//     for (int i = 0; i < n; i++){
-//         scanf("%d", &a[i]);
+    int a[n], mn = INT_MAX, idx;
+    for (int i = 0; i < n; i++){
+        scanf("%d", &a[i]);
 
-//         if(a[i] < mn) {
-//             mn = a[i]; 
-//             idx = i+1;
-//         }
-//     }
-//     printf("%d %d", mn, idx);
+        if(a[i] < mn) {
+            mn = a[i]; 
+            idx = i+1;
+        }
+    }
+    printf("%d %d", mn, idx);
 
-//     return 0;
-// }
+    return 0;
+}
