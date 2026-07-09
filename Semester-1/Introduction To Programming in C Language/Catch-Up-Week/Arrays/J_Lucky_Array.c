84bc1e0215bc01114650edee6962824cc72865f6
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-09 22:46:17 +0600

    feat(Catch-Up-Week/Arrays/J_Lucky_Array.c): add lucky array solution to check odd frequency of minimum element

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Arrays/J_Lucky_Array.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Arrays/J_Lucky_Array.c
@@ -1,29 +1,28 @@
-// #include <stdio.h>
-// #include <limits.h>
+#include <stdio.h>
+#include <limits.h>
 
-// int main()
-// {
-//     int n;
-//     scanf("%d", &n);
+int main(){
+    int n;
+    scanf("%d", &n);
 
-//     int a[n];
-//     for (int i = 0; i < n; i++)
-//         scanf("%d", &a[i]);
+    int a[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
 
-//     int mn = INT_MAX, count = 0;
+    int mn = INT_MAX, count = 0;
 
-//     for (int i = 0; i < n; i++)
-//         if (a[i] < mn)
-//             mn = a[i];
+    for (int i = 0; i < n; i++)
+        if (a[i] < mn)
+            mn = a[i];
 
-//     for (int i = 0; i < n; i++)
-//         if (a[i] == mn)
-//             count++;
+    for (int i = 0; i < n; i++)
+        if (a[i] == mn)
+            count++;
 
-//     if (count % 2 == 1)
-//         printf("Lucky");
-//     else
-//         printf("Unlucky");
+    if (count % 2 == 1)
+        printf("Lucky");
+    else
+        printf("Unlucky");
 
-//     return 0;
-// }
+    return 0;
+}
