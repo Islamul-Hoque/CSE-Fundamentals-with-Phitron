33aa204ff8b2b610f2a96944b270a26c57995069
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-09 22:45:17 +0600

    feat(Catch-Up-Week/Arrays/I_Smallest_Pair.c): add brute-force solution to compute smallest Ai+Aj+(j-i) across all pairs

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Arrays/I_Smallest_Pair.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Arrays/I_Smallest_Pair.c
@@ -1,28 +1,28 @@
-// #include <stdio.h>
-// #include <limits.h>
+#include <stdio.h>
+#include <limits.h>
 
-// int main(){
-//     int t;
-//     scanf("%d", &t);
+int main(){
+    int t;
+    scanf("%d", &t);
 
-//     while (t--){
-//         int n;
-//         scanf("%d", &n);
+    while (t--){
+        int n;
+        scanf("%d", &n);
 
-//         int a[n+1];
-//         for (int i = 1; i <=n; i++)
-//             scanf("%d", &a[i]);
+        int a[n+1];
+        for (int i = 1; i <=n; i++)
+            scanf("%d", &a[i]);
 
-//         int mn = INT_MAX;
+        int mn = INT_MAX;
 
-//         for (int i = 1; i<=n; i++){
-//             for (int j=i+1; j<=n; j++){
-//                 if(a[i]+a[j]+j-i < mn)
-//                     mn = a[i]+a[j]+j-i;
-//             }
-//         }
-//         printf("%d\n", mn);
-//     }
+        for (int i = 1; i<=n; i++){
+            for (int j=i+1; j<=n; j++){
+                if(a[i]+a[j]+j-i < mn)
+                    mn = a[i]+a[j]+j-i;
+            }
+        }
+        printf("%d\n", mn);
+    }
 
-//     return 0;
-// }
+    return 0;
+}
