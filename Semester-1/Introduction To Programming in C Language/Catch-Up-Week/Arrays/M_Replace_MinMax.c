88b66b5bc4b668f3c741e2ee6c87962c11bf42da
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-10 15:09:26 +0600

    feat(Catch-Up-Week/Arrays/M_Replace_MinMax.c): add solution to swap minimum and maximum elements in array

--- Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Arrays/M_Replace_MinMax.c
+++ Semester-1/Introduction To Programming in C Language/Catch-Up-Week/Arrays/M_Replace_MinMax.c
@@ -1,36 +1,35 @@
-// #include <stdio.h>
-// #include <limits.h>
+#include <stdio.h>
+#include <limits.h>
 
-// int main(){
-//     int n;
-//     scanf("%d", &n);
+int main(){
+    int n;
+    scanf("%d", &n);
 
-//     int a[n], flag = 1;
-//     for (int i = 0; i < n; i++)
-//         scanf("%d", &a[i]);
+    int a[n], flag = 1;
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
 
-//     int mn = INT_MAX, mn_idx;
-//     int mx = INT_MIN, mx_idx;
-    
-//     for (int i = 0; i < n; i++){
-//         if(a[i] < mn) {
-//             mn = a[i];
-//             mn_idx = i;
-//         }
-//         if(a[i] > mx) {
-//             mx =a[i];
-//             mx_idx = i;
-//         }
-//     }
+    int mn = INT_MAX, mn_idx;
+    int mx = INT_MIN, mx_idx;
 
-//     a[mn_idx] = mx;
-//     a[mx_idx] = mn;
+    for (int i = 0; i < n; i++){
+        if(a[i] < mn) {
+            mn = a[i];
+            mn_idx = i;
+        }
+        if(a[i] > mx) {
+            mx =a[i];
+            mx_idx = i;
+        }
+    }
 
-//     for (int i = 0; i < n; i++){
-//         printf("%d ", a[i]);
-//     }
+    a[mn_idx] = mx;
+    a[mx_idx] = mn;
 
-//     return 0;
-// }
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
 
 
