361e9eb21a0b7e4aa87362cabf5438bec77941bb
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-13 00:09:13 +0600

    docs(Week-3/Module-9/Quiz.cpp): add quiz questions and answers on doubly linked list concepts

--- Semester-2/Basic Data Structures/Week-3(Doubly Linked List)/Module-9(Doubly Linked List)/Quiz.cpp
+++ Semester-2/Basic Data Structures/Week-3(Doubly Linked List)/Module-9(Doubly Linked List)/Quiz.cpp
@@ -1,41 +1,39 @@
-Q-1: Which of the following statements is true about a doubly linked list?
-Ans: t can be traversed in both directions 
+// Q-1: Which of the following statements is true about a doubly linked list?
+// Ans: It can be traversed in both directions 
 
 
-Q-2:  In a doubly linked list, which pointer of the last node is NULL?
-Ans: next
+// Q-2:  In a doubly linked list, which pointer of the last node is NULL?
+// Ans: next
 
 
-Q-3: In a doubly linked list, each node contains:
-Ans: Both a link to the next and previous nodes 
+// Q-3: In a doubly linked list, each node contains:
+// Ans: Both a link to the next and previous nodes 
 
 
-Q-4: Which of the following is the time complexity for inserting a node at the beginning of a doubly linked list?
-Ans: O(1) 
+// Q-4: Which of the following is the time complexity for inserting a node at the beginning of a doubly linked list?
+// Ans: O(1) 
 
 
-Q-5: Deleting the last node in a doubly linked list takes:
-Ans: O(1) 
+// Q-5: Deleting the last node in a doubly linked list takes:
+// Ans: O(1) 
 
 
-Q-6: Which of the following is the correct way to check if a doubly linked list is empty?
-Ans: Both a and b (Head == NULL && Tail == NULL)
+// Q-6: Which of the following is the correct way to check if a doubly linked list is empty?
+// Ans: Both a and b (Head == NULL && Tail == NULL)
 
 
-Q-7: The process of inserting a new node after a specific node in a doubly linked list involves:
-Ans: Changing the links of both the previous and next nodes 
+// Q-7: The process of inserting a new node after a specific node in a doubly linked list involves:
+// Ans: Changing the links of both the previous and next nodes 
 
 
-Q-8: Which of the following is not an advantage of using a doubly linked list over a singly linked list?
-Ans: Requires less memory space
+// Q-8: Which of the following is not an advantage of using a doubly linked list over a singly linked list?
+// Ans: Requires less memory space
 
 
-Q-9: The process of inserting a new node at the end of a doubly linked list involves:
-Ans: Insert new node after the tail node
+// Q-9: The process of inserting a new node at the end of a doubly linked list involves:
+// Ans: Insert new node after the tail node
 
 
-Q-10: The process of deleting a node from a doubly linked list involves:
-Ans: Changing the links of both the previous and next nodes 
-
-
+// Q-10: The process of deleting a node from a doubly linked list involves:
+// Ans: Changing the links of both the previous and next nodes 
 
