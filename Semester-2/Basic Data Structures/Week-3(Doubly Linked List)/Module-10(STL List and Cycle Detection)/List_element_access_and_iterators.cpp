cf7090e12bb241afb2e0eba412b156ba32a2cb55
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-07-16 22:41:29 +0600

    feat(Week-3/Module-10/List_element_access_and_iterators.cpp): implement element access and iterator functions including front, back, next, begin, and end

--- Semester-2/Basic Data Structures/Week-3(Doubly Linked List)/Module-10(STL List and Cycle Detection)/List_element_access_and_iterators.cpp
+++ Semester-2/Basic Data Structures/Week-3(Doubly Linked List)/Module-10(STL List and Cycle Detection)/List_element_access_and_iterators.cpp
@@ -1,36 +1,36 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main() {
-//     list<int> l = {1, 2, 3, 4, 5};
+int main() {
+    list<int> l = {1, 2, 3, 4, 5};
 
-//     // front() - access the first element
-//     cout<< "First element: " << l.front() << endl;
+    // front() - access the first element
+    cout<< "First element: " << l.front() << endl;
 
-//     // back() - access the last element
-//     cout << "Last element: " << l.back() << endl;
+    // back() - access the last element
+    cout << "Last element: " << l.back() << endl;
 
-//     // next() - access an element using iterator
-//     // next(l.begin(), 2) points to the 3rd element
-//     cout << "Element at index 2: " << *next(l.begin(), 2) << endl;
-
-
-//     // begin() - returns an iterator pointing to the first element
-//     cout << "Element pointed by begin(): "
-//          << *l.begin() << endl;
+    // next() - access an element using iterator
+    // next(l.begin(), 2) points to the 3rd element
+    cout << "Element at index 2: " << *next(l.begin(), 2) << endl;
 
 
-//     // end() - points to the position after the last element
-//     // Therefore, *l.end() is invalid
+    // begin() - returns an iterator pointing to the first element
+    cout << "Element pointed by begin(): "
+         << *l.begin() << endl;
 
-//     auto it = l.end();
-//     --it;
 
-//     cout << "Last element using end(): "
-//          << *it << endl;
+    // end() - points to the position after the last element
+    // Therefore, *l.end() is invalid
 
-//     return 0;
-// }
+    auto it = l.end();
+    --it;
+
+    cout << "Last element using end(): "
+         << *it << endl;
+
+    return 0;
+}
 
 
 
